0e3885e8c4edc1a0933c17c5d102b37c60ed24aa
git config --global user.name Vincentgit config --global user.name <vincentkok@live.nl>
2016-10-11 21:26:16 +0200

    Added the ISO15897 locales

--- Mollie.Api/Models/Payment/Locale.cs
+++ Mollie.Api/Models/Payment/Locale.cs
@@ -1,15 +1,18 @@
-﻿using System.Runtime.Serialization;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
-
-namespace Mollie.Api.Models.Payment {
+﻿namespace Mollie.Api.Models.Payment {
     public static class Locale {
+        public static string de_DE = "de_DE";
+        public static string en_US = "en_US";
+        public static string es_ES = "es_ES";
+        public static string fr_FR = "fr_FR";
+        public static string nl_BE = "nl_BE";
+        public static string fr_BE = "fr_BE";
+        public static string nl_NL = "nl_BE";
+
         public static string DE = "de";
         public static string EN = "en";
         public static string ES = "es";
         public static string FR = "fr";
         public static string BE = "be";
-        public static string BEFR = "be-fr";
         public static string NL = "nl";
     }
 }
