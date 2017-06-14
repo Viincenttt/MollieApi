0c49dbca5fe3d450d548b6e0dd7d4d637647caa9
Lex Goudriaan <LexGoudriaan@outlook.com>
2017-06-14 16:31:28 +0200

    Mode enum jsonconverter, Baseclient resharper clean-up, added simple list

--- Mollie.Api/Client/BaseMollieClient.cs
+++ Mollie.Api/Client/BaseMollieClient.cs
@@ -9,8 +9,10 @@ using Mollie.Api.JsonConverters;
 using Newtonsoft.Json;
 using RestSharp;
 
-namespace Mollie.Api.Client {
-    public abstract class BaseMollieClient {
+namespace Mollie.Api.Client
+{
+    public abstract class BaseMollieClient
+    {
         public const string ApiEndPoint = "https://api.mollie.nl";
         public const string ApiVersion = "v1";
 
@@ -18,8 +20,10 @@ namespace Mollie.Api.Client {
         private readonly RestClient _restClient;
         private readonly JsonSerializerSettings _defaultJsonDeserializerSettings;
 
-        public BaseMollieClient(string apiKey) {
-            if (string.IsNullOrEmpty(apiKey)) {
+        protected BaseMollieClient(string apiKey)
+        {
+            if (string.IsNullOrEmpty(apiKey))
+            {
                 throw new ArgumentException("Mollie API key cannot be empty");
             }
 
@@ -28,46 +32,59 @@ namespace Mollie.Api.Client {
             this._restClient = this.CreateRestClient();
         }
 
-        protected async Task<T> GetAsync<T>(string relativeUri) {
-            RestRequest request = new RestRequest(relativeUri, Method.GET);
+        protected async Task<T> GetAsync<T>(string relativeUri)
+        {
+            var request = new RestRequest(relativeUri, Method.GET);
             return await this.ExecuteRequestAsync<T>(request).ConfigureAwait(false);
         }
 
-        protected async Task<T> GetListAsync<T>(string relativeUri, int? offset, int? count) {
-            RestRequest request = new RestRequest(relativeUri, Method.GET);
-            if (offset.HasValue) {
+        protected async Task<T> GetListAsync<T>(string relativeUri, int? offset, int? count)
+        {
+            var request = new RestRequest(relativeUri, Method.GET);
+            if (offset.HasValue)
+            {
                 request.AddParameter("offset", offset);
             }
-            if (count.HasValue) {
+            if (count.HasValue)
+            {
                 request.AddParameter("count", count);
             }
 
             return await this.ExecuteRequestAsync<T>(request).ConfigureAwait(false);
         }
 
-        protected async Task<T> PostAsync<T>(string relativeUri, object data) {
-            RestRequest request = new RestRequest(relativeUri, Method.POST);
-            request.AddParameter(String.Empty, JsonConvertExtensions.SerializeObjectCamelCase(data), ParameterType.RequestBody);
+        protected async Task<T> PostAsync<T>(string relativeUri, object data)
+        {
+            var request = new RestRequest(relativeUri, Method.POST);
+
+            if (data != null)
+                request.AddParameter(string.Empty, JsonConvertExtensions.SerializeObjectCamelCase(data), ParameterType.RequestBody);
 
             return await this.ExecuteRequestAsync<T>(request).ConfigureAwait(false);
         }
 
-        protected async Task DeleteAsync(string relativeUri) {
-            RestRequest request = new RestRequest(relativeUri, Method.DELETE);
+        protected async Task DeleteAsync(string relativeUri)
+        {
+            var request = new RestRequest(relativeUri, Method.DELETE);
             await this.ExecuteRequestAsync<object>(request).ConfigureAwait(false);
         }
 
-        private async Task<T> ExecuteRequestAsync<T>(IRestRequest request) {
-            IRestResponse response = await this._restClient.ExecuteTaskAsync(request).ConfigureAwait(false);
+        private async Task<T> ExecuteRequestAsync<T>(IRestRequest request)
+        {
+            var response = await this._restClient.ExecuteTaskAsync(request).ConfigureAwait(false);
             return this.ProcessHttpResponseMessage<T>(response);
         }
 
-        private T ProcessHttpResponseMessage<T>(IRestResponse response) {
-            if (response.IsSuccessful()) {
+        private T ProcessHttpResponseMessage<T>(IRestResponse response)
+        {
+            if (response.IsSuccessful())
+            {
                 return JsonConvert.DeserializeObject<T>(response.Content, this._defaultJsonDeserializerSettings);
             }
-            else {
-                switch (response.StatusCode) {
+            else
+            {
+                switch (response.StatusCode)
+                {
                     case HttpStatusCode.BadRequest:
                     case HttpStatusCode.Unauthorized:
                     case HttpStatusCode.Forbidden:
@@ -85,8 +102,9 @@ namespace Mollie.Api.Client {
         /// <summary>
         /// Creates a new rest client for the Mollie API
         /// </summary>
-        private RestClient CreateRestClient() {
-            RestClient restClient = new RestClient();
+        private RestClient CreateRestClient()
+        {
+            var restClient = new RestClient();
             restClient.BaseUrl = this.GetBaseAddress();
             restClient.AddDefaultHeader("Content-Type", "application/json");
             restClient.AddDefaultParameter("Authorization", $"Bearer {this._apiKey}", ParameterType.HttpHeader);
@@ -104,8 +122,10 @@ namespace Mollie.Api.Client {
         /// Creates the default Json serial settings for the JSON.NET parsing.
         /// </summary>
         /// <returns></returns>
-        private JsonSerializerSettings CreateDefaultJsonDeserializerSettings() {
-            return new JsonSerializerSettings {
+        private JsonSerializerSettings CreateDefaultJsonDeserializerSettings()
+        {
+            return new JsonSerializerSettings
+            {
                 DateFormatString = "MM-dd-yyyy",
                 NullValueHandling = NullValueHandling.Ignore,
                 Converters = new List<JsonConverter>() {
