e19f2bba7d45283e65f4ea8186e70c905c748dfb
Lex Goudriaan <lexgoudriaan@outlook.com>
2017-10-15 22:43:26 +0200

    Connect api models + Interface

---
+++ Mollie.Api/Client/Abstract/IConnectClient.cs
@@ -0,0 +1,11 @@
+﻿using Mollie.Api.Models.Connect;
+
+namespace Mollie.Api.Client.Abstract
+{
+	public interface IConnectClient
+	{
+		string GetAuthorizationUrl(AuthorizeRequest authorizeRequest);
+
+		TokenResponse GetAccessToken(TokenRequest tokenRequest);
+	}
+}
