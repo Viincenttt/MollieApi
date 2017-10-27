4407a05ce95e695e42d54f7b1578b724ac121153
Lex Goudriaan <LexGoudriaan@outlook.com>
2017-10-27 16:57:48 +0200

    Connectclient implementation

---
+++ Mollie.Api/Client/ConnectClient.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Mollie.Api.Client.Abstract;
+using Mollie.Api.ContractResolvers;
+using Mollie.Api.Extensions;
+using Mollie.Api.Models.Connect;
+using Newtonsoft.Json;
+
+namespace Mollie.Api.Client
+{
+	public class ConnectClient : IConnectClient
+	{
+		private const string BaseUrl = "https://www.mollie.com/oauth2/";
+		private readonly HttpClient _httpClient;
+		private readonly string _clientId;
+
+		public ConnectClient(string clientId, string clientSecret)
+		{
+			if (string.IsNullOrWhiteSpace(clientId))
+				throw new ArgumentNullException(nameof(clientId));
+
+			if (string.IsNullOrWhiteSpace(clientSecret))
+				throw new ArgumentNullException(nameof(clientSecret));
+
+			_clientId = clientId;
+			_httpClient = CreateHttpClient(clientId, clientSecret);
+		}
+
+		public string GetAuthorizationUrl(string state, List<string> scopes, string redirectUri = null, bool forceApprovalPrompt = false)
+		{
+			var parameters = new Dictionary<string, string>()
+			{
+				{ "client_id", _clientId },
+				{ "state", state },
+				{ "scope", string.Join(" ", scopes) },
+				{ "response_type", "code" },
+				{ "approval_prompt", forceApprovalPrompt ? "force" : "auto" }
+			};
+
+			if (!string.IsNullOrWhiteSpace(redirectUri))
+				parameters.Add("redirect_uri", redirectUri);
+
+			return BaseUrl + "authorize?" + string.Join("&", parameters.Select(p => string.Format($"{WebUtility.UrlEncode(p.Key)}={WebUtility.UrlEncode(p.Value)}")));
+		}
+
+		public async Task<TokenResponse> GetAccessTokenAsync(TokenRequest request)
+		{
+			var jsonData = JsonConvertExtensions.SerializeObjectSnakeCase(request);
+			var response = await this._httpClient.PostAsync(BaseUrl + "tokens", new StringContent(jsonData, Encoding.UTF8, "application/json")).ConfigureAwait(false);
+			var resultContent = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+			return JsonConvert.DeserializeObject<TokenResponse>(resultContent, new JsonSerializerSettings() { ContractResolver = new SnakeCasePropertyNamesContractResolver() });
+		}
+
+		/// <summary>
+		/// Creates a new rest client for the Mollie API with basic authentication
+		/// </summary>
+		private HttpClient CreateHttpClient(string clientId, string clientSecret)
+		{
+			var httpClient = new HttpClient();
+			httpClient.DefaultRequestHeaders.Accept.Clear();
+			httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+			httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Base64Encode($"{clientId}:{clientSecret}"));
+			return httpClient;
+		}
+
+		private string Base64Encode(string value)
+		{
+			var bytes = Encoding.UTF8.GetBytes(value);
+			return Convert.ToBase64String(bytes);
+		}
+	}
+}
--- Mollie.Api/Models/Connect/TokenRequest.cs
+++ Mollie.Api/Models/Connect/TokenRequest.cs
@@ -2,25 +2,40 @@
 {
 	public class TokenRequest
 	{
+		public TokenRequest(string code, string redirectUri)
+		{
+			if (code.StartsWith("refresh_"))
+			{
+				GrantType = "refresh_token";
+				RefreshToken = code;
+			}
+			else
+			{
+				GrantType = "authorization_code";
+				Code = code;
+			}
+			RedirectUri = redirectUri;
+		}
+
 		/// <summary>
 		/// If you wish to exchange your auth code for an access token, use grant type authorization_code. If you wish to renew your access token with your refresh token, use grant type refresh_token.
 		/// Possible values: authorization_code refresh_token
 		/// </summary>
-		public string GrantType { get; set; }
+		public string GrantType { get; }
 
 		/// <summary>
 		/// Optional – The auth code you've received when creating the authorization. Only use this field when using grant type authorization_code.
 		/// </summary>
-		public string Code { get; set; }
+		public string Code { get; }
 
 		/// <summary>
 		/// Optional – The refresh token you've received when creating the authorization. Only use this field when using grant type refresh_token.
 		/// </summary>
-		public string RefreshToken { get; set; }
+		public string RefreshToken { get; }
 
 		/// <summary>
 		/// The URL the merchant is sent back to once the request has been authorized. It must match the URL you set when registering your app.
 		/// </summary>
-		public string RedirectUri { get; set; }
+		public string RedirectUri { get; }
 	}
 }
