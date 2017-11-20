18f705ac4bc3ed4042872c2d126e059face01eb0
Lex Goudriaan <lexgoudriaan@outlook.com>
2017-11-20 14:54:49 +0100

    Added the permissions api

---
+++ Mollie.Api/Client/PermissionsClient.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Mollie.Api.Client.Abstract;
+using Mollie.Api.Models.List;
+using Mollie.Api.Models.Permission;
+
+namespace Mollie.Api.Client
+{
+	public class PermissionsClient : OauthBaseMollieClient, IPermissionsClient
+	{
+		public PermissionsClient(string oauthAccessToken) : base(oauthAccessToken) { }
+
+		public async Task<PermissionResponse> GetPermissionAsync(string id)
+		{
+			return await this.GetAsync<PermissionResponse>($"permissions/{id}").ConfigureAwait(false);
+		}
+
+		public async Task<ListResponse<PermissionResponse>> GetPermissionListAsync(int? offset = null, int? count = null)
+		{
+			return await this.GetListAsync<ListResponse<PermissionResponse>>("permissions", offset, count).ConfigureAwait(false);
+		}
+	}
+}
