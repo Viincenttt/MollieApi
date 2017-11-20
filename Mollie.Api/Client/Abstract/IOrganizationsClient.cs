4d18f93933b279e847f0c6fc8001e6b35c007c48
Lex Goudriaan <lexgoudriaan@outlook.com>
2017-11-20 15:06:40 +0100

    Added Organizations api

---
+++ Mollie.Api/Client/Abstract/IOrganizationsClient.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Mollie.Api.Models.Organization;
+
+namespace Mollie.Api.Client.Abstract
+{
+	public interface IOrganizationsClient
+	{
+		Task<OrganizationResponse> GetOrganizationAsync(string id);
+	}
+}
---
+++ Mollie.Api/Client/OrganizationsClient.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Mollie.Api.Client.Abstract;
+using Mollie.Api.Models.Organization;
+
+namespace Mollie.Api.Client
+{
+	public class OrganizationsClient : OauthBaseMollieClient, IOrganizationsClient
+	{
+		public OrganizationsClient(string oauthAccessToken) : base(oauthAccessToken) { }
+
+		public async Task<OrganizationResponse> GetOrganizationAsync(string id)
+		{
+			return await this.GetAsync<OrganizationResponse>($"organizations/{id}").ConfigureAwait(false);
+		}
+	}
+}
