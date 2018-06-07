267f3f02efb0247baaa000de1d4a910db083bcbb
Vincent <vincentkok@live.nl>
2018-06-07 19:49:10 +0200

    Readded bancontact payment response type

---
+++ Mollie.Api/Models/Payment/Response/Specific/BancontactPaymentResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mollie.Api.Models.Payment.Response.Specific {
+    public class BancontactPaymentResponse : PaymentResponse {
+        public BancontactPaymentResponseDetails Details { get; set; }
+    }
+
+    public class BancontactPaymentResponseDetails {
+        public string CardNumber { get; set; }
+    }
+}
