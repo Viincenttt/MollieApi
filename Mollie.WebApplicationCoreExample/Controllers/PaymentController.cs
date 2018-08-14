7be520acc21706fb561e119dde47f8c284cd0201
Vincent <vincentkok@live.nl>
2018-08-14 19:51:21 +0200

    Added a view to display a list of payments

---
+++ Mollie.WebApplicationCoreExample/Controllers/PaymentController.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Mollie.Api.Client.Abstract;
+using Mollie.Api.Models.List;
+using Mollie.Api.Models.List.Specific;
+
+namespace Mollie.WebApplicationCoreExample.Controllers {
+    public class PaymentController : Controller {
+        private readonly IPaymentClient _paymentClient;
+
+        public PaymentController(IPaymentClient paymentClient) {
+            this._paymentClient = paymentClient;
+        }
+
+        public async Task<ViewResult> Index() {
+            ListResponse<PaymentListData> paymentList = await this._paymentClient.GetPaymentListAsync();
+            return this.View(paymentList);
+        }
+    }
+}
