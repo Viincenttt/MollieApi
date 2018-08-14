60eb0b1f139633c301f8fcc2cd4331046c6017df
Vincent <vincentkok@live.nl>
2018-08-14 19:33:27 +0200

    Moved Mollie API service registration to middleware namespace/folder

--- Mollie.WebApplicationCoreExample/Middleware/MollieApiClientServiceExtensions.cs
+++ Mollie.WebApplicationCoreExample/Middleware/MollieApiClientServiceExtensions.cs
@@ -2,8 +2,8 @@
 using Mollie.Api.Client;
 using Mollie.Api.Client.Abstract;
 
-namespace Mollie.WebApplicationCoreExample.Extensions {
-    public static class ServiceCollectionExtensions {
+namespace Mollie.WebApplicationCoreExample.Middleware {
+    public static class MollieApiClientServiceExtensions {
         public static IServiceCollection AddMollieApi(this IServiceCollection services, string apiKey) {
             services.AddSingleton<IPaymentClient, PaymentClient>(x => new PaymentClient(apiKey));
 
--- Mollie.WebApplicationCoreExample/Startup.cs
+++ Mollie.WebApplicationCoreExample/Startup.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Mollie.WebApplicationCoreExample.Extensions;
+using Mollie.WebApplicationCoreExample.Middleware;
 
 namespace Mollie.WebApplicationCoreExample {
     public class Startup {
@@ -18,6 +18,8 @@ namespace Mollie.WebApplicationCoreExample {
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+            // TODO: Add middleware to validate Mollie API key and display an error if a live/invalid api key is passed
+
             app.UseDeveloperExceptionPage();
             app.UseStaticFiles();
             app.UseMvcWithDefaultRoute();
