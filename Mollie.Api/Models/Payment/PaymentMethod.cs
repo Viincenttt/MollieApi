c8dda7d87ca24fc96fe40082ebfe55472639b5ea
Vincent <vincentkok@live.nl>
2018-08-14 19:03:50 +0200

    Added support for the eps and giropay payment methods

--- Mollie.Api/Models/Payment/PaymentMethod.cs
+++ Mollie.Api/Models/Payment/PaymentMethod.cs
@@ -5,19 +5,21 @@ using Newtonsoft.Json.Converters;
 namespace Mollie.Api.Models.Payment {
     [JsonConverter(typeof(StringEnumConverter))]
     public enum PaymentMethod {
-        [EnumMember(Value = "ideal")] Ideal,
-        [EnumMember(Value = "creditcard")] CreditCard,
         [EnumMember(Value = "bancontact")] Bancontact,
-        [EnumMember(Value = "sofort")] Sofort,
         [EnumMember(Value = "banktransfer")] BankTransfer,
-        [EnumMember(Value = "directdebit")] DirectDebit,
         [EnumMember(Value = "belfius")] Belfius,
         [EnumMember(Value = "bitcoin")] Bitcoin,
+        [EnumMember(Value = "creditcard")] CreditCard,
+        [EnumMember(Value = "directdebit")] DirectDebit,
+        [EnumMember(Value = "eps")] Eps,
+        [EnumMember(Value = "giftcard")] GiftCard,
+        [EnumMember(Value = "giropay ")] Giropay,
+        [EnumMember(Value = "ideal")] Ideal,
+        [EnumMember(Value = "inghomepay")] IngHomePay,
+        [EnumMember(Value = "kbc")] Kbc,
         [EnumMember(Value = "paypal")] PayPal,
         [EnumMember(Value = "paysafecard")] PaySafeCard,
-        [EnumMember(Value = "kbc")] Kbc,
-        [EnumMember(Value = "giftcard")] GiftCard,
-        [EnumMember(Value = "inghomepay")] IngHomePay,
+        [EnumMember(Value = "sofort")] Sofort,
         [EnumMember(Value = "refund")] Refund,
     }
 }
