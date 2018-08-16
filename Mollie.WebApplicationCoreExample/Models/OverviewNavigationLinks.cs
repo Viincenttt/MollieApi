299119c0cb836eb7bed4d80b03168fd664ee215a
Vincent <vincentkok@live.nl>
2018-08-17 00:03:08 +0200

    Simplified automapper mappings

--- Mollie.WebApplicationCoreExample/Models/OverviewNavigationLinks.cs
+++ Mollie.WebApplicationCoreExample/Models/OverviewNavigationLinks.cs
@@ -4,5 +4,10 @@ namespace Mollie.WebApplicationCoreExample.Models {
     public class OverviewNavigationLinks {
         public UrlLink Previous { get; set; }
         public UrlLink Next { get; set; }
+
+        public OverviewNavigationLinks(UrlLink previous, UrlLink next) {
+            this.Previous = previous;
+            this.Next = next;
+        }
     }
 }
--- Mollie.WebApplicationCoreExample/Services/Automapper/WebsiteProfile.cs
+++ Mollie.WebApplicationCoreExample/Services/Automapper/WebsiteProfile.cs
@@ -13,16 +13,9 @@ namespace Mollie.WebApplicationCoreExample.Services.Automapper {
             this.CreateMap<CreatePaymentModel, PaymentRequest>()
                 .ForMember(x => x.Amount, m => m.MapFrom(x => new Amount(x.Currency, x.Amount.ToString(CultureInfo.InvariantCulture))));
 
-            this.CreateMap<CreatePaymentModel, Amount>()
-                .ForMember(x => x.Value, m => m.MapFrom(x => x.Amount));
-
             this.CreateMap<ListResponse<PaymentListData>, OverviewModel<PaymentResponse>>()
                 .ForMember(x => x.Items, m => m.MapFrom(x => x.Embedded.Payments))
-                .ForMember(x => x.Navigation, m => m.MapFrom(x => x));
-
-            this.CreateMap<ListResponse<PaymentListData>, OverviewNavigationLinks>()
-                .ForMember(x => x.Previous, m => m.MapFrom(x => x.Links.Previous))
-                .ForMember(x => x.Next, m => m.MapFrom(x => x.Links.Next));
+                .ForMember(x => x.Navigation, m => m.MapFrom(x => new OverviewNavigationLinks(x.Links.Previous, x.Links.Next)));
         }
     }
 }
