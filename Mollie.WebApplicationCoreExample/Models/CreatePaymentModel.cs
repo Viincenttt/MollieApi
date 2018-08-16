3192e87141237759ae83451f4720aff973d43582
Vincent <vincentkok@live.nl>
2018-08-16 22:58:25 +0200

    Added regular expression validator to make sure the amount has 2 decimals

--- Mollie.WebApplicationCoreExample/Models/CreatePaymentModel.cs
+++ Mollie.WebApplicationCoreExample/Models/CreatePaymentModel.cs
@@ -3,7 +3,8 @@
 namespace Mollie.WebApplicationCoreExample.Models {
     public class CreatePaymentModel {
         [Required]
-        [Range(0.01, 1000, ErrorMessage = "Please enter a number between 0.01 and 1000")]
+        [Range(0.01, 1000, ErrorMessage = "Please enter an amount between 0.01 and 1000")]
+        [RegularExpression(@"\d+(\.\d{2})?", ErrorMessage = "Please enter an amount with two decimal places")]
         public decimal Amount { get; set; }
 
         [Required]
