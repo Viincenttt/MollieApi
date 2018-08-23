66b6bb65e9edc1580ed3f366dad7dd10672714a1
Vincent <vincentkok@live.nl>
2018-08-23 22:30:36 +0200

    Added service registrations for all Mollie API clients

--- Mollie.WebApplicationCoreExample/Framework/Middleware/MollieApiClientServiceExtensions.cs
+++ Mollie.WebApplicationCoreExample/Framework/Middleware/MollieApiClientServiceExtensions.cs
@@ -5,8 +5,13 @@ using Mollie.Api.Client.Abstract;
 namespace Mollie.WebApplicationCoreExample.Framework.Middleware {
     public static class MollieApiClientServiceExtensions {
         public static IServiceCollection AddMollieApi(this IServiceCollection services, string apiKey) {
-            services.AddSingleton<IPaymentClient, PaymentClient>(x => new PaymentClient(apiKey));
-            services.AddSingleton<ICustomerClient, CustomerClient>(x => new CustomerClient(apiKey));
+            services.AddScoped<IPaymentClient, PaymentClient>(x => new PaymentClient(apiKey));
+            services.AddScoped<ICustomerClient, CustomerClient>(x => new CustomerClient(apiKey));
+            services.AddScoped<IRefundClient, RefundClient>(x => new RefundClient(apiKey));
+            services.AddScoped<IPaymentMethodClient, PaymentMethodClient>(x => new PaymentMethodClient(apiKey));
+            services.AddScoped<ISubscriptionClient, SubscriptionClient>(x => new SubscriptionClient(apiKey));
+            services.AddScoped<IMandateClient, MandateClient>(x => new MandateClient(apiKey));
+            services.AddScoped<IInvoicesClient, InvoicesClient>(x => new InvoicesClient(apiKey));
 
             return services;
         }
