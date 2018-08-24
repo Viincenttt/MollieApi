08d7ee13462b275d708e80f76c3d35630adff69c
Vincent <vincentkok@live.nl>
2018-08-24 14:59:50 +0200

    Deleted the old obsolete web application example

--- Mollie.WebApplicationExample/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Mollie.WebApplicationExample.MvcApplication" Language="C#" %>
--- Mollie.WebApplicationExample/Models/SubscriptionListViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-using Mollie.Api.Models.Subscription;
-
-namespace Mollie.WebApplicationExample.Models {
-    public class SubscriptionListViewModel {
-        public string CustomerId { get; set; }
-        public IList<SubscriptionResponse> Subscriptions { get; set; }
-    }
-}
