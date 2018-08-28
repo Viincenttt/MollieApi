3bb04a07d792f88b5bb64b1234617e12701d7a6d
Vincent <vincentkok@live.nl>
2018-08-28 19:21:07 +0200

    Removed obsolete IMollieClient interface and tests, since these are no longer used

--- Mollie.Api/Client/Abstract/IMollieClient.cs
+++
@@ -1,4 +0,0 @@
-﻿namespace Mollie.Api.Client.Abstract {
-    public interface IMollieClient : ICustomerClient, IMandateClient, IPaymentClient, IPaymentMethodClient, IRefundClient, ISubscriptionClient {
-    }
-}
