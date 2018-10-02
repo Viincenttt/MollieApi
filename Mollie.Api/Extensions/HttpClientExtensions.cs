746646c12a83ac381297f026e357558a627bf6ad
Vincent <vincentkok@live.nl>
2018-10-02 19:36:27 +0200

    #67 Added extension methods to add patch support to HttpClient

---
+++ Mollie.Api/Extensions/HttpClientExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Mollie.Api.Extensions {
+    public static class HttpClientExtensions {
+        public static Task<HttpResponseMessage> PatchAsync(this HttpClient client, string requestUri, HttpContent content) {
+            return client.PatchAsync(CreateUri(requestUri), content);
+        }
+
+        public static Task<HttpResponseMessage> PatchAsync(this HttpClient client, Uri requestUri, HttpContent content) {
+            return client.PatchAsync(requestUri, content, CancellationToken.None);
+        }
+
+        public static Task<HttpResponseMessage> PatchAsync(this HttpClient client, string requestUri, HttpContent content, CancellationToken cancellationToken) {
+            return client.PatchAsync(CreateUri(requestUri), content, cancellationToken);
+        }
+
+        public static Task<HttpResponseMessage> PatchAsync(this HttpClient client, Uri requestUri, HttpContent content, CancellationToken cancellationToken) {
+            return client.SendAsync(new HttpRequestMessage(new HttpMethod("PATCH"), requestUri) { Content = content }, cancellationToken);
+        }
+
+        private static Uri CreateUri(string uri) {
+            return string.IsNullOrEmpty(uri) ? null : new Uri(uri, UriKind.RelativeOrAbsolute);
+        }
+    }
+}
