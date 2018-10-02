1be0b526a24ea4a410cf66d5c1b92161de5016af
Vincent <vincentkok@live.nl>
2018-10-02 19:25:17 +0200

    Added the StringEnumConverter JsonConverter attribute to the FeeRegion, CardSecurity and FailureReason properties of the CreditCardPaymentResponseDetails class

--- Mollie.Api/Models/Payment/Response/Specific/CreditCardPaymentResponse.cs
+++ Mollie.Api/Models/Payment/Response/Specific/CreditCardPaymentResponse.cs
@@ -43,21 +43,24 @@ namespace Mollie.Api.Models.Payment.Response {
         /// The ISO 3166-1 alpha-2 country code of the country the card was issued in. For example: BE.
         /// </summary>
         public string CardCountryCode { get; set; }
-        
+
         /// <summary>
         /// Only available if the payment succeeded. – The payment's security type.
         /// </summary>
+        [JsonConverter(typeof(StringEnumConverter))]
         public CreditCardSecurity? CardSecurity { get; set; }
-        
+
         /// <summary>
         /// Only available if the payment succeeded. – The fee region for the payment. See your credit card addendum for
         /// details. intra-eu for consumer cards from the EU, and other for all other cards.
         /// </summary>
+        [JsonConverter(typeof(StringEnumConverter))]
         public CreditCardFeeRegion? FeeRegion { get; set; }
 
         /// <summary>
         /// Only available for failed payments. Contains a failure reason code.
         /// </summary>
+        [JsonConverter(typeof(StringEnumConverter))]
         public CreditCardFailureReason? FailureReason { get; set; }
     }
 
