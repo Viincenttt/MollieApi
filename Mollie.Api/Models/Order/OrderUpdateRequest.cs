306d94d3c70e245c6050695bb21d59b8ce6cbb39
Vincent <vincentkok@live.nl>
2018-10-05 00:03:13 +0200

    #79 Added the OrderUpdateRequest class

---
+++ Mollie.Api/Models/Order/OrderUpdateRequest.cs
@@ -0,0 +1,15 @@
+﻿namespace Mollie.Api.Models.Order {
+    public class OrderUpdateRequest {
+        /// <summary>
+        /// The billing person and address for the order. See Order address details for the
+        /// exact fields needed.
+        /// </summary>
+        public OrderAddressDetails BillingAddress { get; set; }
+
+        /// <summary>
+        /// The shipping address for the order. See Order address details for the exact
+        /// fields needed.
+        /// </summary>
+        public OrderAddressDetails ShippingAddress { get; set; }
+    }
+}
