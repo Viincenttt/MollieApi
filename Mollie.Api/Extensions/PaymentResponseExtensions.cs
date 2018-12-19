073cd2d5c411dac6aee6571b58bd296daf3834ce
Neil Boyd <NeilBoyd@gmail.com>
2018-12-19 22:45:03 +0100

    extension method to get derived payment statuses

---
+++ Mollie.Api/Extensions/PaymentResponseExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace Mollie.Api.Extensions
+{
+    public static class PaymentResponseExtensions
+    {
+        public static bool IsRefunded(this PaymentResponse paymentResponse)
+        {
+            var amountRefunded = paymentResponse?.AmountRefunded?.Value;
+            return !string.IsNullOrEmpty(amountRefunded) && amountRefunded != "0.00";
+        }
+
+        public static bool IsChargeback(this PaymentResponse paymentResponse)
+        {
+            var chargebacks = paymentResponse?.Links?.Chargebacks;
+            return chargebacks != null;
+        }
+    }
+}
