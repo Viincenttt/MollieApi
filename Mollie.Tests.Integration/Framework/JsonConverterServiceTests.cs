826d2e28d0b815fb6997d2742feed30077a715ca
Vincent <vincentkok@live.nl>
2019-02-07 20:10:35 +0100

    #99 Added unit tests to test all possible metadata cases

---
+++ Mollie.Tests.Integration/Framework/JsonConverterServiceTests.cs
@@ -0,0 +1,54 @@
+﻿using Mollie.Api.Framework;
+using Mollie.Api.Models.Payment.Response;
+using NUnit.Framework;
+
+namespace Mollie.Tests.Integration.Framework {
+    [TestFixture]
+    public class JsonConverterServiceTests {
+        [Test]
+        public void CanDeserializeJsonMetadata() {
+            // Given: A JSON metadata value
+            JsonConverterService jsonConverterService = new JsonConverterService();
+            string metadataJson = @"{
+  ""ReferenceNumber"": null,
+  ""OrderID"": null,
+  ""UserID"": ""534721""
+}";
+            string paymentJson = @"{""metadata"":" + metadataJson + "}";
+
+            // When: We deserialize the JSON
+            PaymentResponse payments = jsonConverterService.Deserialize<PaymentResponse>(paymentJson);
+
+            // Then: 
+            Assert.AreEqual(metadataJson, payments.Metadata);
+        }
+
+        [Test]
+        public void CanDeserializeStringMetadataValue() {
+            // Given: A JSON metadata value
+            JsonConverterService jsonConverterService = new JsonConverterService();
+            string metadataJson = "This is my metadata";
+            string paymentJson = @"{""metadata"":""" + metadataJson + @"""}";
+
+            // When: We deserialize the JSON
+            PaymentResponse payments = jsonConverterService.Deserialize<PaymentResponse>(paymentJson);
+
+            // Then: 
+            Assert.AreEqual(metadataJson, payments.Metadata);
+        }
+
+        [Test]
+        public void CanDeserializeNullMetadataValue() {
+            // Given: A JSON metadata value
+            JsonConverterService jsonConverterService = new JsonConverterService();
+            string metadataJson = @"null";
+            string paymentJson = @"{""metadata"":" + metadataJson + "}";
+
+            // When: We deserialize the JSON
+            PaymentResponse payments = jsonConverterService.Deserialize<PaymentResponse>(paymentJson);
+
+            // Then: 
+            Assert.AreEqual(null, payments.Metadata);
+        }
+    }
+}
