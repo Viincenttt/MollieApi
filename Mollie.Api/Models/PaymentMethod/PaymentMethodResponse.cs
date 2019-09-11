6f3896b4b42580a0a2c7eb113fd0fae0395c8e69
Bert Kooij <bert.kooij@certigon.nl>
2019-09-11 08:53:56 +0200

    Added ammounts for payment method

--- Mollie.Api/Models/PaymentMethod/PaymentMethodResponse.cs
+++ Mollie.Api/Models/PaymentMethod/PaymentMethodResponse.cs
@@ -21,6 +21,16 @@ namespace Mollie.Api.Models.PaymentMethod {
         public string Description { get; set; }
 
         /// <summary>
+        /// Minimum payment amount required to use this payment method.
+        /// </summary>
+        public Amount MinimumAmount { get; set; }
+
+        /// <summary>
+        /// Maximum payment amount allowed when using this payment method. (Could be null)
+        /// </summary>
+        public Amount MaximumAmount { get; set; }
+
+        /// <summary>
         /// URLs of images representing the payment method.
         /// </summary>
         public PaymentMethodResponseImage Image { get; set; }
