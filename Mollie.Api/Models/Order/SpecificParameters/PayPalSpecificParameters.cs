4c7c94176d447c82216eabccae94d9506cabb5e3
Bert Kooij <bert.kooij@certigon.nl>
2019-09-11 08:46:44 +0200

    Implementation for Payment Specific Parameters

---
+++ Mollie.Api/Models/Order/SpecificParameters/PayPalSpecificParameters.cs
@@ -0,0 +1,16 @@
+﻿namespace Mollie.Api.Models.Order.SpecificParameters
+{
+    public class PayPalSpecificParameters : PaymentSpecificParameters
+    {
+        /// <summary>
+        /// Description for Paypal order. Use keywords Cart, Order, Invoice or Payment followed by order ID.
+        /// </summary>
+        public string Description { get; set; }
+
+
+        /// <summary>
+        /// Optional - The shipping address details.
+        /// </summary>
+        public AddressObject ShippingAddress { get; set; }
+    }
+}
