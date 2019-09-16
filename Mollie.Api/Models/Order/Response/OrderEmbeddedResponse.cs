efb2f9af8578859c7aed77bbec21aa5376df7f82
Bert Kooij <bert.kooij@certigon.nl>
2019-09-16 09:10:25 +0200

    Add embed parameters for Order

---
+++ Mollie.Api/Models/Order/Response/OrderEmbeddedResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Mollie.Api.Extensions;
+using Mollie.Api.JsonConverters;
+using Mollie.Api.Models.Payment;
+using Mollie.Api.Models.Payment.Response;
+using Mollie.Api.Models.Refund;
+using Mollie.Api.Models.Subscription;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Mollie.Api.Models.Order {
+
+    public class OrderEmbeddedResponse : IResponseObject {
+
+        public IEnumerable<PaymentResponse> Payments { get; set; }
+
+        public IEnumerable<RefundResponse> Refunds { get; set; }
+
+    }
+}
