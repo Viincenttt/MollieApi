e12f6f6b32880405235286c0644fb2244f0d928f
Vincent Kok <vincentkok@live.nl>
2019-11-22 19:59:42 +0100

    Add embed options to order client (#156)
    * Add embed parameters for Order (#144)
    
    * Use existing code conventions

---
+++ Mollie.Api/Models/Order/Response/OrderEmbeddedResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Mollie.Api.Models.Payment.Response;
+using Mollie.Api.Models.Refund;
+
+namespace Mollie.Api.Models.Order {
+
+    public class OrderEmbeddedResponse : IResponseObject {
+
+        public IEnumerable<PaymentResponse> Payments { get; set; }
+
+        public IEnumerable<RefundResponse> Refunds { get; set; }
+    }
+}
