70577664003dd88306272b16ed09a407437d639d
Peter Verswyvelen <bugfact@gmail.com>
2020-03-19 14:34:05 +0100

    Made decimal places validation culture invariant

--- Mollie.WebApplicationCoreExample/Models/CreatePaymentModel.cs
+++ Mollie.WebApplicationCoreExample/Models/CreatePaymentModel.cs
@@ -2,11 +2,13 @@
 using Mollie.Api.Models;
 using Mollie.WebApplicationCoreExample.Framework.Validators;
 
-namespace Mollie.WebApplicationCoreExample.Models {
-    public class CreatePaymentModel {
+namespace Mollie.WebApplicationCoreExample.Models
+{
+    public class CreatePaymentModel
+    {
         [Required]
         [Range(0.01, 1000, ErrorMessage = "Please enter an amount between 0.01 and 1000")]
-        [RegularExpression(@"\d+(\.\d{2})?", ErrorMessage = "Please enter an amount with two decimal places")]
+        [DecimalPlaces(2, ErrorMessage = "Please enter an amount with two decimal places")]
         public decimal Amount { get; set; }
 
         [Required]
