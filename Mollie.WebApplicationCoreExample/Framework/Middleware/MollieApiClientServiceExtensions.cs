f511ec4604d8843ef5e6a559980285378f7eec48
Vincent <vincentkok@live.nl>
2020-04-22 19:38:22 +0200

    Use strongly typed Mollie configuration in ASP.NET project

--- Mollie.WebApplicationCoreExample/Framework/Middleware/MollieApiClientServiceExtensions.cs
+++ Mollie.WebApplicationCoreExample/Framework/Middleware/MollieApiClientServiceExtensions.cs
@@ -1,18 +1,22 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Mollie.Api.Client;
 using Mollie.Api.Client.Abstract;
+using Mollie.Api.Framework;
 
 namespace Mollie.WebApplicationCoreExample.Framework.Middleware {
     public static class MollieApiClientServiceExtensions {
-        public static IServiceCollection AddMollieApi(this IServiceCollection services, string apiKey) {
+        public static IServiceCollection AddMollieApi(this IServiceCollection services, IConfiguration configuration) {
+            MollieConfiguration mollieConfiguration = configuration.GetSection("Mollie").Get<MollieConfiguration>();
+
             services.AddScoped<IPaymentClient, PaymentClient>();
-            services.AddScoped<IPaymentClient, PaymentClient>(x => new PaymentClient(apiKey));
-            services.AddScoped<ICustomerClient, CustomerClient>(x => new CustomerClient(apiKey));
-            services.AddScoped<IRefundClient, RefundClient>(x => new RefundClient(apiKey));
-            services.AddScoped<IPaymentMethodClient, PaymentMethodClient>(x => new PaymentMethodClient(apiKey));
-            services.AddScoped<ISubscriptionClient, SubscriptionClient>(x => new SubscriptionClient(apiKey));
-            services.AddScoped<IMandateClient, MandateClient>(x => new MandateClient(apiKey));
-            services.AddScoped<IInvoicesClient, InvoicesClient>(x => new InvoicesClient(apiKey));
+            services.AddScoped<IPaymentClient, PaymentClient>(x => new PaymentClient(mollieConfiguration.ApiKey));
+            services.AddScoped<ICustomerClient, CustomerClient>(x => new CustomerClient(mollieConfiguration.ApiKey));
+            services.AddScoped<IRefundClient, RefundClient>(x => new RefundClient(mollieConfiguration.ApiKey));
+            services.AddScoped<IPaymentMethodClient, PaymentMethodClient>(x => new PaymentMethodClient(mollieConfiguration.ApiKey));
+            services.AddScoped<ISubscriptionClient, SubscriptionClient>(x => new SubscriptionClient(mollieConfiguration.ApiKey));
+            services.AddScoped<IMandateClient, MandateClient>(x => new MandateClient(mollieConfiguration.ApiKey));
+            services.AddScoped<IInvoicesClient, InvoicesClient>(x => new InvoicesClient(mollieConfiguration.ApiKey));
 
             return services;
         }
