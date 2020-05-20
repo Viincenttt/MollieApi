e73b669f8dc008f47c991171157cad2af79a2ba2
Leon Mastenbroek <leon.mastenbroek@outlook.com>
2020-05-20 15:19:48 +0200

    - Added implicit cast operator for Amount to decimal. - Added new constructor which makes it able to construct Amount with a decimal value.

--- Mollie.Api/Models/Amount.cs
+++ Mollie.Api/Models/Amount.cs
@@ -1,5 +1,10 @@
-﻿namespace Mollie.Api.Models {
-    public class Amount {
+﻿using System;
+using System.Globalization;
+
+namespace Mollie.Api.Models
+{
+    public class Amount
+    {
         /// <summary>
         /// An ISO 4217 currency code. The currencies supported depend on the payment methods that are enabled on your account.
         /// </summary>
@@ -10,14 +15,34 @@
         /// </summary>
         public string Value { get; set; }
 
-        public Amount(string currency, string value) {
+        public Amount(string currency, string value)
+        {
             this.Currency = currency;
             this.Value = value;
         }
 
+        /// <summary>
+        /// Constructor for constructing based on a decimal value
+        /// </summary>
+        /// <param name="currency"></param>
+        /// <param name="value"></param>
+        public Amount(string currency, decimal value)
+        {
+            this.Currency = currency;
+            this.Value = value.ToString("0.00", CultureInfo.InvariantCulture);
+        }
+
         public Amount() { }
 
-        public override string ToString() {
+        /// <summary>
+        /// Implicit cast operator from Amount to decimal.
+        /// </summary>
+        /// <param name="amount"></param>
+        public static implicit operator decimal(Amount amount)
+            => Decimal.TryParse(amount.Value, NumberStyles.Number, CultureInfo.InvariantCulture, out var a) ? a : throw new InvalidCastException($"Cannot convert {amount.Value} to decimal");
+
+        public override string ToString()
+        {
             return $"{this.Value} {this.Currency}";
         }
     }
