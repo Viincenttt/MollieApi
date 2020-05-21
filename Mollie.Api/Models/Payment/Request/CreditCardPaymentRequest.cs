4c430fa08e9937c90266613ecd12d1505b1af29d
Keven van Zuijlen <kevenvz1@gmail.com>
2020-05-21 11:28:38 +0200

    Extend payment requests (#181)

--- Mollie.Api/Models/Payment/Request/CreditCardPaymentRequest.cs
+++ Mollie.Api/Models/Payment/Request/CreditCardPaymentRequest.cs
@@ -15,5 +15,11 @@ namespace Mollie.Api.Models.Payment.Request {
         /// protection, and thus improve conversion.
         /// </summary>
         public AddressObject ShippingAddress { get; set; }
+
+        /// <summary>
+        /// The card token you get from Mollie Components. The token contains the card information
+        /// (such as card holder, card number and expiry date) needed to complete the payment.
+        /// </summary>
+        public string CardToken { get; set; }
     }
 }
