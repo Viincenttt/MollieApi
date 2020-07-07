ca2d18aed1a37ba1eeb406c7b62e639c336c9426
Vincent <vincentkok@live.nl>
2020-07-07 13:43:58 +0200

    #132 - Add unit test to verify that metadata values are serialized in the correct format

--- Mollie.Tests.Unit/Framework/JsonConverterServiceTests.cs
+++ Mollie.Tests.Unit/Framework/JsonConverterServiceTests.cs
@@ -1,10 +1,31 @@
 ﻿using Mollie.Api.Framework;
+using Mollie.Api.Models;
+using Mollie.Api.Models.Payment.Request;
 using Mollie.Api.Models.Payment.Response;
 using NUnit.Framework;
 
 namespace Mollie.Tests.Unit.Framework {
     [TestFixture]
     public class JsonConverterServiceTests {
+        [Test]
+        public void Serialize_JsonData_IsSerialized() {
+            // Given: A JSON metadata value
+            JsonConverterService jsonConverterService = new JsonConverterService();
+            PaymentRequest paymentRequest = new PaymentRequest() {
+                Amount = new Amount(Currency.EUR, "100.00"),
+                Description = "Description",
+                RedirectUrl = "http://www.mollie.com",
+                Metadata = "{\"firstName\":\"John\",\"lastName\":\"Doe\"}",
+            };
+            string expectedJsonValue = "{\"amount\":{\"currency\":\"EUR\",\"value\":\"100.00\"},\"description\":\"Description\",\"redirectUrl\":\"http://www.mollie.com\",\"metadata\":{\"firstName\":\"John\",\"lastName\":\"Doe\"}}";
+
+            // When: We serialize the JSON
+            string jsonValue = jsonConverterService.Serialize(paymentRequest);
+
+            // Then:            
+            Assert.AreEqual(expectedJsonValue, jsonValue);
+        }
+
         [Test]
         public void Deserialize_JsonData_IsDeserialized() {
             // Given: A JSON metadata value
