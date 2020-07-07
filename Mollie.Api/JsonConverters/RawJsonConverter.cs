03bedeca1ba5df3b33445f720d7e5ba1e67c9bea
Vincent <vincentkok@live.nl>
2020-07-07 13:43:33 +0200

    #132 - Fix bug in RawJsonConverter where it could not handle non-json metadata values

--- Mollie.Api/JsonConverters/RawJsonConverter.cs
+++ Mollie.Api/JsonConverters/RawJsonConverter.cs
@@ -9,11 +9,41 @@ namespace Mollie.Api.JsonConverters {
         }
 
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer) {
-            writer.WriteRawValue(value.ToString());
+            string valueToParse = value.ToString();
+            if (IsValidJson(valueToParse)) {
+                writer.WriteRawValue(valueToParse);
+            }
+            else {
+                writer.WriteValue(valueToParse);
+            }            
         }
 
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer) {
             return JToken.Load(reader).ToString();
+        }
+
+        /// <summary>
+        /// Source: https://stackoverflow.com/questions/14977848/how-to-make-sure-that-string-is-valid-json-using-json-net
+        /// </summary>
+        private bool IsValidJson(string strInput) {
+            strInput = strInput.Trim();
+            if ((strInput.StartsWith("{") && strInput.EndsWith("}")) ||
+                (strInput.StartsWith("[") && strInput.EndsWith("]")))
+            {
+                try {
+                    var obj = JToken.Parse(strInput);
+                    return true;
+                }
+                catch (JsonReaderException) {
+                    return false;
+                }
+                catch (Exception) {
+                    return false;
+                }
+            }
+            else {
+                return false;
+            }
         }
     }
 }
