2f87a6135ad2b2a119725585755560fcfb408b8f
Vincent <vincentkok@live.nl>
2020-07-08 11:21:27 +0200

    Add support for the "payments" and “profile”  properties in the "_links" object of the SubscriptionResponseLinks class

--- Mollie.Api/Models/Subscription/SubscriptionResponseLinks.cs
+++ Mollie.Api/Models/Subscription/SubscriptionResponseLinks.cs
@@ -1,4 +1,7 @@
 ﻿using Mollie.Api.Models.Customer;
+using Mollie.Api.Models.List;
+using Mollie.Api.Models.Payment.Response;
+using Mollie.Api.Models.Profile.Response;
 using Mollie.Api.Models.Url;
 
 namespace Mollie.Api.Models.Subscription {
@@ -12,6 +15,17 @@ namespace Mollie.Api.Models.Subscription {
         /// The API resource URL of the customer the subscription is for.
         /// </summary>
         public UrlObjectLink<CustomerResponse> Customer { get; set; }
+
+        /// <summary>
+        /// The API resource URL of the payments that are created by this subscription. Not present 
+        /// if no payments yet created.
+        /// </summary>
+        public UrlObjectLink<ListResponse<PaymentResponse>> Payments { get; set; }
+
+        /// <summary>
+        /// The API resource URL of the website profile on which this subscription was created.
+        /// </summary>
+        public UrlObjectLink<ProfileResponse> Profile { get; set; }
 
         /// <summary>
         /// The URL to the subscription retrieval endpoint documentation.
