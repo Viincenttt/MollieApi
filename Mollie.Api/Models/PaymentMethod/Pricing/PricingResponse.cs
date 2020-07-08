a98164b843521f508f12fb26578cbd8c8c1a9922
Vincent <vincentkok@live.nl>
2020-07-08 10:39:14 +0200

    Add support for the FeeRegion property in the PricingResponse of the PaymentMethodResponse class

--- Mollie.Api/Models/PaymentMethod/Pricing/PricingResponse.cs
+++ Mollie.Api/Models/PaymentMethod/Pricing/PricingResponse.cs
@@ -15,5 +15,12 @@
         /// A string containing the percentage what will be charged over the payment amount besides the fixed price.
         /// </summary>
         public decimal Variable { get; set; }
+
+        /// <summary>
+        /// This value is only available for credit card rates. It will correspond with the regions as documented in 
+        /// the Payments API. See the Mollie.Api.Models.Payment.Response.CreditCardFeeRegion class for a full list of 
+        /// known values.
+        /// </summary>
+        public string FeeRegion { get; set; }
     }
 }
