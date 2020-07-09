87c8c5959f2adba4593e8f71f94c5c74088227df
Vincent <vincentkok@live.nl>
2020-07-09 19:00:07 +0200

    Add a integration test to verify we can retrieve the current profile from the ProfileClient

---
+++ Mollie.Tests.Integration/Api/ProfileTests.cs
@@ -0,0 +1,23 @@
+﻿using Mollie.Api.Models.Profile.Response;
+using Mollie.Tests.Integration.Framework;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace Mollie.Tests.Integration.Api {
+    [TestFixture]
+    public class ProfileTests : BaseMollieApiTestClass {
+        [Test]
+        public async Task GetCurrentProfileAsync_ReturnsCurrentProfile() {
+            // Given
+
+            // When: We retrieve the current profile from the mollie API
+            ProfileResponse profileResponse = await this._profileClient.GetCurrentProfileAsync();
+
+            // Then: Make sure we get a valid response
+            Assert.IsNotNull(profileResponse);
+            Assert.IsNotNull(profileResponse.Id);
+            Assert.IsNotNull(profileResponse.Email);
+            Assert.IsNotNull(profileResponse.Status);
+        }
+    }
+}
