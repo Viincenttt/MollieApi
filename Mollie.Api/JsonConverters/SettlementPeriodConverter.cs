27fd254ea23c9fa0fde8cbc17e6eb387d123eea2
Vincent <vincentkok@live.nl>
2020-11-11 20:05:43 +0100

    #197 Add a custom JsonConverter to parse the SettlementPeriod response, as this can be a object or an empty array

---
+++ Mollie.Api/JsonConverters/SettlementPeriodConverter.cs
@@ -0,0 +1,29 @@
+﻿using Mollie.Api.Models.Settlement;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+
+namespace Mollie.Api.JsonConverters {
+    public class SettlementPeriodConverter : JsonConverter {
+        public override bool CanConvert(Type objectType) {
+            return false;
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer) {
+            // If we have no periods, Mollie returns a empty array instead of an object      
+            JToken token = JToken.Load(reader);
+            if (token is JArray) {
+                return new Dictionary<int, Dictionary<int, SettlementPeriod>>();
+            }
+
+            existingValue = existingValue ?? serializer.ContractResolver.ResolveContract(objectType).DefaultCreator();
+            serializer.Populate(token.CreateReader(), existingValue);
+            return existingValue;
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer) {
+            throw new NotImplementedException();
+        }
+    }
+}
