9d96d338be0d5b60ea790276eaf38b48ab0c2c5e
Vincent <vincentkok@live.nl>
2020-11-16 20:05:38 +0100

    Make the testmode parameter in the RefundClient not nullable

--- Mollie.Api/Client/RefundClient.cs
+++ Mollie.Api/Client/RefundClient.cs
@@ -21,13 +21,13 @@ namespace Mollie.Api.Client {
             return await this.PostAsync<RefundResponse>($"payments/{paymentId}/refunds", refundRequest).ConfigureAwait(false);
         }
 
-        public async Task<ListResponse<RefundResponse>> GetRefundListAsync(string from = null, int? limit = null, bool? testmode = default) {
+        public async Task<ListResponse<RefundResponse>> GetRefundListAsync(string from = null, int? limit = null, bool testmode = false) {
             var queryParameters = this.BuildQueryParameters(testmode: testmode);
             
             return await this.GetListAsync<ListResponse<RefundResponse>>($"refunds", from, limit, queryParameters).ConfigureAwait(false);
         }
         
-        public async Task<ListResponse<RefundResponse>> GetRefundListAsync(string paymentId, string from = null, int? limit = null, bool? testmode = default) {
+        public async Task<ListResponse<RefundResponse>> GetRefundListAsync(string paymentId, string from = null, int? limit = null, bool testmode = false) {
             var queryParameters = this.BuildQueryParameters(testmode: testmode);
 
             return await this.GetListAsync<ListResponse<RefundResponse>>($"payments/{paymentId}/refunds", from, limit, queryParameters).ConfigureAwait(false);
@@ -37,17 +37,17 @@ namespace Mollie.Api.Client {
             return await this.GetAsync(url).ConfigureAwait(false);
         }
 
-        public async Task<RefundResponse> GetRefundAsync(string paymentId, string refundId, bool? testmode = default) {
+        public async Task<RefundResponse> GetRefundAsync(string paymentId, string refundId, bool testmode = false) {
             var queryParameters = this.BuildQueryParameters(testmode: testmode);
             return await this.GetAsync<RefundResponse>($"payments/{paymentId}/refunds/{refundId}{queryParameters.ToQueryString()}").ConfigureAwait(false);
         }
 
-        public async Task CancelRefundAsync(string paymentId, string refundId, bool? testmode = default) {
+        public async Task CancelRefundAsync(string paymentId, string refundId, bool testmode = default) {
             var queryParameters = this.BuildQueryParameters(testmode: testmode);
             await this.DeleteAsync($"payments/{paymentId}/refunds/{refundId}{queryParameters.ToQueryString()}").ConfigureAwait(false);
         }
         
-        private Dictionary<string, string> BuildQueryParameters(bool? testmode = false) {
+        private Dictionary<string, string> BuildQueryParameters(bool testmode = false) {
             var result = new Dictionary<string, string>();
             result.AddValueIfTrue(nameof(testmode), testmode);
             return result;
