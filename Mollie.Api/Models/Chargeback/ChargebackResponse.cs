90084ee5b4c17917f3cb9d34fd1acc633a9022ba
Sippelberg <35916517+Sippelberg@users.noreply.github.com>
2021-05-20 22:53:08 +0200

    Update ChargebackResponse.cs (#225)

--- Mollie.Api/Models/Chargeback/ChargebackResponse.cs
+++ Mollie.Api/Models/Chargeback/ChargebackResponse.cs
@@ -3,10 +3,10 @@ using Newtonsoft.Json;
 
 namespace Mollie.Api.Models.Chargeback {
     public class ChargebackResponse : IResponseObject {
-		/// <summary>
-		/// The chargeback's unique identifier, for example chb_n9z0tp.
-		/// </summary>
-		public string Id { get; set; }
+	/// <summary>
+	/// The chargeback's unique identifier, for example chb_n9z0tp.
+	/// </summary>
+	public string Id { get; set; }
 
         /// <summary>
         /// The amount charged back.
@@ -32,7 +32,12 @@ namespace Mollie.Api.Models.Chargeback {
         /// <summary>
         /// The id of the payment this chargeback belongs to.
         /// </summary>
-		public string PaymentId { get; set; }
+	public string PaymentId { get; set; }
+	    
+	/// <summary>
+        /// The reason given for a Chargeback, this can help determine the cost for the chargeback
+        /// </summary>
+        public ChargebackResponseReason ChargebackResponseReason { get; set; }
 
         /// <summary>
         /// An object with several URL objects relevant to the chargeback. Every URL object will contain an href and a type field.
