f1083a849db2f0ee71623eb466ff6082a9b58520
ErikvdBurgwal <erikvandeburgwal@gmail.com>
2021-06-11 08:30:02 +0200

    Add PaymentLinkClient implementation for new mollie API feature (#240)
    Co-authored-by: Erik van de Burgwal <info@erikvandeburgwal.nl>

---
+++ Mollie.Api/Client/Abstract/IPaymentLinkClient.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using Mollie.Api.Models.List;
+using Mollie.Api.Models.PaymentLink.Request;
+using Mollie.Api.Models.PaymentLink.Response;
+using Mollie.Api.Models.Url;
+
+namespace Mollie.Api.Client.Abstract {
+    public interface IPaymentLinkClient {
+        /*
+            https://docs.mollie.com/reference/v2/payment-links-api/create-payment-link
+            https://docs.mollie.com/reference/v2/payment-links-api/list-payment-links
+            https://docs.mollie.com/reference/v2/payment-links-api/get-payment-link
+         */
+        Task<PaymentLinkResponse> CreatePaymentLinkAsync(PaymentLinkRequest paymentLinkRequest);
+
+        /// <summary>
+        ///	Retrieve a single payment link object by its token.
+        /// </summary>
+        /// <param name="paymentLinkId">The payment link's ID, for example pl_4Y0eZitmBnQ6IDoMqZQKh.</param>
+        ///// <param name="testmode">Oauth - Optional – Set this to true to get a payment link made in test mode. If you omit this parameter, you can only retrieve live mode paymentLinks.</param>
+        /// <returns></returns>
+        Task<PaymentLinkResponse> GetPaymentLinkAsync(string paymentLinkId, bool testmode = false);
+         
+        /// <summary>
+        /// Retrieve all payment links created with the current payment link profile, ordered from newest to oldest.
+        /// </summary>
+        /// <param name="from"></param>
+        /// <param name="limit"></param>
+        /// <returns></returns>
+		Task<ListResponse<PaymentLinkResponse>> GetPaymentLinkListAsync(string from = null, int? limit = null, string profileId = null, bool testmode = false);
+        Task<ListResponse<PaymentLinkResponse>> GetPaymentLinkListAsync(UrlObjectLink<ListResponse<PaymentLinkResponse>> url);
+        Task<PaymentLinkResponse> GetPaymentLinkAsync(UrlObjectLink<PaymentLinkResponse> url); 
+    }
+}
---
+++ Mollie.Api/Client/PaymentLinkClient.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Mollie.Api.Client.Abstract;
+using Mollie.Api.Extensions;
+using Mollie.Api.Models.List;
+using Mollie.Api.Models.PaymentLink.Request;
+using Mollie.Api.Models.PaymentLink.Response;
+using Mollie.Api.Models.Url;
+
+namespace Mollie.Api.Client
+{
+    public class PaymentLinkClient : BaseMollieClient, IPaymentLinkClient
+    {
+
+        public PaymentLinkClient(string apiKey, HttpClient httpClient = null) : base(apiKey, httpClient) { }
+
+        public async Task<PaymentLinkResponse> CreatePaymentLinkAsync(PaymentLinkRequest paymentRequest)
+        {
+            if (!string.IsNullOrWhiteSpace(paymentRequest.ProfileId) || paymentRequest.Testmode.HasValue /*|| paymentRequest.ApplicationFee != null*/)
+            {
+                this.ValidateApiKeyIsOauthAccesstoken();
+            }
+            return await this.PostAsync<PaymentLinkResponse>($"payment-links", paymentRequest).ConfigureAwait(false);
+        }
+
+        public async Task<PaymentLinkResponse> GetPaymentLinkAsync(string paymentLinkId, bool testmode = false)
+        {
+            if (testmode)
+            {
+                this.ValidateApiKeyIsOauthAccesstoken();
+            }
+
+            var queryParameters = this.BuildQueryParameters(
+                testmode: testmode);
+
+            return await this.GetAsync<PaymentLinkResponse>($"payment-links/{paymentLinkId}{queryParameters.ToQueryString()}").ConfigureAwait(false);
+        }
+         
+        public async Task<PaymentLinkResponse> GetPaymentLinkAsync(UrlObjectLink<PaymentLinkResponse> url)
+        {
+            return await this.GetAsync(url).ConfigureAwait(false);
+        }
+         
+        
+        public async Task<ListResponse<PaymentLinkResponse>> GetPaymentLinkListAsync(UrlObjectLink<ListResponse<PaymentLinkResponse>> url)
+        {
+            return await this.GetAsync(url).ConfigureAwait(false);
+        }
+ 
+        public async Task<ListResponse<PaymentLinkResponse>> GetPaymentLinkListAsync(string from = null, int? limit = null, string profileId = null, bool testmode = false)
+        {
+            if (!string.IsNullOrWhiteSpace(profileId) || testmode)
+            {
+                this.ValidateApiKeyIsOauthAccesstoken();
+            }
+
+            var queryParameters = this.BuildQueryParameters(
+               profileId: profileId,
+               testmode: testmode);
+
+            return await this.GetListAsync<ListResponse<PaymentLinkResponse>>($"payment-links", from, limit, queryParameters).ConfigureAwait(false);
+        }
+
+        private Dictionary<string, string> BuildQueryParameters(string profileId = null, bool testmode = false)
+        {
+            var result = new Dictionary<string, string>();
+            result.AddValueIfTrue(nameof(testmode), testmode);
+            result.AddValueIfNotNullOrEmpty(nameof(profileId), profileId);
+            return result;
+        }
+    }
+}
