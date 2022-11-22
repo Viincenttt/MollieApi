c61e5753335af7bb6ffb7e9c2c7bd9e041aa5b94
Vincent <vincentkok@live.nl>
2022-11-22 21:06:46 +0100

    #282 Add unit test for ListBalancesAsync of BalanceClient

--- Mollie.Tests.Unit/Client/BalanceClientTests.cs
+++ Mollie.Tests.Unit/Client/BalanceClientTests.cs
@@ -76,6 +76,115 @@ namespace Mollie.Tests.Unit.Client {
           Assert.AreEqual(getBalanceResponseFactory.TransferDestination.BeneficiaryName, balanceResponse.TransferDestination.BeneficiaryName);
       }
 
+      [Test]
+      public async Task ListBalancesAsync_DefaultBehaviour_ResponseIsParsed() {
+          // Given: We request a list of balances
+          string expectedUrl = $"{BaseMollieClient.ApiEndPoint}balances";
+          var mockHttp = this.CreateMockHttpMessageHandler(HttpMethod.Get, expectedUrl, DefaultListBalancesResponse);
+          HttpClient httpClient = mockHttp.ToHttpClient();
+          BalanceClient balanceClient = new BalanceClient("api-key", httpClient);
+
+          // When: We make the request
+          var balances = await balanceClient.ListBalancesAsync();
+
+          // Then: Response should be parsed
+          mockHttp.VerifyNoOutstandingExpectation();
+          Assert.IsNotNull(balances);
+          Assert.AreEqual(2, balances.Count);
+          Assert.AreEqual(2, balances.Items.Count);
+      }
+      
+      private readonly string DefaultListBalancesResponse = $@"{{
+  ""count"": 2,
+  ""_embedded"": {{
+    ""balances"": [
+       {{
+         ""resource"": ""balance"",
+         ""id"": ""bal_gVMhHKqSSRYJyPsuoPNFH"",
+         ""mode"": ""live"",
+         ""createdAt"": ""2019-01-10T12:06:28+00:00"",
+         ""currency"": ""EUR"",
+         ""status"": ""active"",
+         ""availableAmount"": {{
+           ""value"": ""0.00"",
+           ""currency"": ""EUR""
+         }},
+         ""pendingAmount"": {{
+           ""value"": ""0.00"",
+           ""currency"": ""EUR""
+         }},
+         ""transferFrequency"": ""daily"",
+         ""transferThreshold"": {{
+           ""value"": ""40.00"",
+           ""currency"": ""EUR""
+         }},
+         ""transferReference"": ""Mollie payout"",
+         ""transferDestination"": {{
+           ""type"": ""bank-account"",
+           ""beneficiaryName"": ""Jack Bauer"",
+           ""bankAccount"": ""NL53INGB0654422370"",
+           ""bankAccountId"": ""bnk_jrty3f""
+         }},
+         ""_links"": {{
+           ""self"": {{
+             ""href"": ""https://api.mollie.com/v2/balances/bal_gVMhHKqSSRYJyPsuoPNFH"",
+             ""type"": ""application/hal+json""
+           }}
+         }}
+       }},
+       {{
+         ""resource"": ""balance"",
+         ""id"": ""bal_gVMhHKqSSRYJyPsuoPABC"",
+         ""mode"": ""live"",
+         ""createdAt"": ""2019-01-10T10:23:41+00:00"",
+         ""status"": ""active"",
+         ""currency"": ""EUR"",
+         ""availableAmount"": {{
+           ""value"": ""0.00"",
+           ""currency"": ""EUR""
+         }},
+         ""pendingAmount"": {{
+           ""value"": ""0.00"",
+           ""currency"": ""EUR""
+         }},
+         ""transferFrequency"": ""twice-a-month"",
+         ""transferThreshold"": {{
+           ""value"": ""5.00"",
+           ""currency"": ""EUR""
+         }},
+         ""transferReference"": ""Mollie payout"",
+         ""transferDestination"": {{
+           ""type"": ""bank-account"",
+           ""beneficiaryName"": ""Jack Bauer"",
+           ""bankAccount"": ""NL97MOLL6351480700"",
+           ""bankAccountId"": ""bnk_jrty3e""
+         }},
+         ""_links"": {{
+           ""self"": {{
+             ""href"": ""https://api.mollie.com/v2/balances/bal_gVMhHKqSSRYJyPsuoPABC"",
+             ""type"": ""application/hal+json""
+           }}
+         }}
+       }}
+    ]
+  }},
+  ""_links"": {{
+    ""documentation"": {{
+      ""href"": ""https://docs.mollie.com/reference/v2/balances-api/list-balances"",
+      ""type"": ""text/html""
+    }},
+    ""self"": {{
+      ""href"": ""https://api.mollie.com/v2/balances?limit=5"",
+      ""type"": ""application/hal+json""
+    }},
+    ""previous"": null,
+    ""next"": {{
+      ""href"": ""https://api.mollie.com/v2/balances?from=bal_gVMhHKqSSRYJyPsuoPABC&limit=5"",
+      ""type"": ""application/hal+json""
+    }}
+  }}
+}}";
+
       private class GetBalanceResponseFactory {
           public string BalanceId { get; set; } = "bal_gVMhHKqSSRYJyPsuoPNFH";
           public DateTime CreatedAt { get; set; } = DateTime.SpecifyKind(new DateTime(2022, 11, 22, 13, 15, 0), DateTimeKind.Utc);
