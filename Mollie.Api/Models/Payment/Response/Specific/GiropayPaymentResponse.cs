8a8b72887d3bdcb90297798e15eaa4b8cf1a60e8
wouter <wouter@dirks-it.nl>
2022-12-06 12:05:01 +0100

    Add Giropay and Eps PaymentResponseDetails

---
+++ Mollie.Api/Models/Payment/Response/Specific/GiropayPaymentResponse.cs
@@ -0,0 +1,28 @@
+﻿namespace Mollie.Api.Models.Payment.Response
+{
+    public class GiropayPaymentResponse : PaymentResponse
+    {
+        /// <summary>
+        /// An object with the consumer bank account details.
+        /// </summary>
+        public GiropayPaymentResponseDetails Details { get; set; }
+    }
+
+    public class GiropayPaymentResponseDetails
+    {
+        /// <summary>
+        /// Only available if the payment has been completed – The consumer's name.
+        /// </summary>
+        public string ConsumerName { get; set; }
+
+        /// <summary>
+        /// Only available if the payment has been completed – The consumer's IBAN.
+        /// </summary>
+        public string ConsumerAccount { get; set; }
+
+        /// <summary>
+        /// Only available if the payment has been completed – The consumer's bank's BIC.
+        /// </summary>
+        public string ConsumerBic { get; set; }
+    }
+}
