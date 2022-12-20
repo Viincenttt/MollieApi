c5a66efec2df628ed50fd75e690dd9626bc30f36
Vincent <vincentkok@live.nl>
2022-12-20 18:46:50 +0100

    Restructure status balance report object structure

--- Mollie.Api/Models/Balance/Response/Specific/StatusBalance/StatusBalanceReportResponse.cs
+++ Mollie.Api/Models/Balance/Response/Specific/StatusBalance/StatusBalanceReportResponse.cs
@@ -1,5 +1,32 @@
 ﻿namespace Mollie.Api.Models.Balance.Response.Specific.StatusBalance {
     public class StatusBalanceReportResponse : BalanceReportResponse {
+        public StatusBalancesTotal Totals { get; set; }
+    }
+    
+    public class StatusBalancesTotal {
+        public StatusBalancesPendingBalance PendingBalance { get; set; }
+        public StatusBalanceAvailableBalance AvailableBalance { get; set; }
+    }
+
+    public class StatusBalancesPendingBalance {
+        public StatusBalanceAmount Open { get; set; }
+        public StatusBalanceAmount Pending { get; set; }
+        public StatusBalanceAmount MovedToAvailable { get; set; }
+        public StatusBalanceAmount Close { get; set; }
+    }
+
+    public class StatusBalanceAvailableBalance {
+        public StatusBalanceAmount Open { get; set; }
+        public StatusBalanceAmount MovedFromPending { get; set; }
+        public StatusBalanceAmount ImmediatelyAvailable { get; set; }
+        public StatusBalanceAmount Close { get; set; }
+    }
+
+    public class StatusBalanceAmount {
+        public Amount Amount { get; set; }
         
+        public override string ToString() {
+            return this.Amount.ToString();
+        }
     }
 }
