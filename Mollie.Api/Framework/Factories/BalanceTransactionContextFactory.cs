5b280592a5eee0eabe00a5fd51838ecdf68fce1d
Vincent <vincentkok@live.nl>
2022-12-21 18:38:01 +0100

    #282 Create BalanceTransactionContextFactory

---
+++ Mollie.Api/Framework/Factories/BalanceTransactionContextFactory.cs
@@ -0,0 +1,35 @@
+﻿using Mollie.Api.Models.Balance.Response.BalanceTransaction;
+using Mollie.Api.Models.Balance.Response.BalanceTransaction.Specific;
+
+namespace Mollie.Api.Framework.Factories {
+    public class BalanceTransactionContextFactory {
+        public BaseTransactionContext Create(string type) {
+            switch (type) {
+                case BalanceTransactionContextType.Payment:
+                case BalanceTransactionContextType.UnauthorizedDirectDebit:
+                case BalanceTransactionContextType.FailedPayment:
+                case BalanceTransactionContextType.ChargebackReversal:
+                case BalanceTransactionContextType.ApplicationFee:
+                case BalanceTransactionContextType.SplitPayment:
+                    return new PaymentTransactionContext();
+                case BalanceTransactionContextType.Capture:
+                    return new CaptureTransactionContext();
+                case BalanceTransactionContextType.Refund:
+                case BalanceTransactionContextType.ReturnedRefund:
+                case BalanceTransactionContextType.PlatformPaymentRefund:
+                    return new RefundTransactionContext();
+                case BalanceTransactionContextType.Chargeback:
+                case BalanceTransactionContextType.PlatformPaymentChargeback:
+                    return new ChargebackTransactionContext();
+                case BalanceTransactionContextType.OutgoingTransfer:
+                case BalanceTransactionContextType.CanceledOutgoingTransfer:
+                case BalanceTransactionContextType.ReturnedTransfer:
+                    return new SettlementTransactionContext();
+                case BalanceTransactionContextType.InvoiceCompensation:
+                    return new InvoiceTransactionContext();
+                default: 
+                    return new BaseTransactionContext();
+            }
+        }
+    }
+}
--- Mollie.Api/Models/Balance/Response/BalanceTransaction/BalanceTransactionResponse.cs
+++ Mollie.Api/Models/Balance/Response/BalanceTransaction/BalanceTransactionResponse.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Mollie.Api.Models.Balance.Response.BalanceTransaction.Specific;
 using Newtonsoft.Json;
 
 namespace Mollie.Api.Models.Balance.Response.BalanceTransaction {
@@ -65,5 +66,31 @@ namespace Mollie.Api.Models.Balance.Response.BalanceTransaction {
         /// The date and time of the movement, in ISO 8601 format.
         /// </summary>
         public DateTime CreatedAt { get; set; }
+        
+        /// <summary>
+        /// Depending on the type of the balance transaction, we will try to give more context about the specific event
+        /// that triggered the movement. 
+        /// </summary>
+        public BaseTransactionContext Context { get; set; }
+    }
+
+    public static class BalanceTransactionContextType {
+        public const string Payment = "payment";
+        public const string Capture = "capture";
+        public const string UnauthorizedDirectDebit = "unauthorized-direct-debit";
+        public const string FailedPayment = "failed-payment";
+        public const string Refund = "refund";
+        public const string ReturnedRefund = "returned-refund";
+        public const string Chargeback = "chargeback";
+        public const string ChargebackReversal = "chargeback-reversal";
+        public const string OutgoingTransfer = "outgoing-transfer";
+        public const string CanceledOutgoingTransfer = "canceled-outgoing-transfer";
+        public const string ReturnedTransfer = "returned-transfer";
+        public const string InvoiceCompensation = "invoice-compensation";
+        public const string BalanceCorrection = "balance-correction";
+        public const string ApplicationFee = "application-fee";
+        public const string SplitPayment = "split-payment";
+        public const string PlatformPaymentRefund = "platform-payment-refund";
+        public const string PlatformPaymentChargeback = "platform-payment-chargeback";
     }
 }
