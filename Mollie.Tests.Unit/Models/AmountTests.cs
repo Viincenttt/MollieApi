d6c8d5966aa969b10efd508f7b6449c7c4cbd2da
Vincent <vincentkok@live.nl>
2023-02-06 15:55:56 +0100

    #293 Add unit tests for JPY and ISK amount conversion

---
+++ Mollie.Tests.Unit/Models/AmountTests.cs
@@ -0,0 +1,19 @@
+﻿using Mollie.Api.Models;
+using NUnit.Framework;
+
+namespace Mollie.Tests.Unit.Models {
+    public class AmountTests {
+        [TestCase("EUR", 50.25, "50.25")]
+        [TestCase("EUR", 50, "50.00")]
+        [TestCase("JPY", 51, "51")]
+        [TestCase("ISK", 52, "52")]
+        [TestCase("ISK", 52.40, "52")]
+        public void CreateAmount_DecimalIsConverted_ValueHasCorrectFormat(string currency, decimal value, string expectedResult) {
+            // Arrange & act
+            Amount amount = new Amount(currency, value);
+            
+            // Assert
+            Assert.AreEqual(expectedResult, amount.Value);
+        }
+    }
+}
