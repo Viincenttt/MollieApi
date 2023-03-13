b96704597fba3f1a986d3a430a80189e0a402d93
Vincent <vincentkok@live.nl>
2023-03-13 19:43:36 +0100

    #295 Validate required URL parameters in ProfileClient

--- Mollie.Api/Client/ProfileClient.cs
+++ Mollie.Api/Client/ProfileClient.cs
@@ -16,6 +16,7 @@ namespace Mollie.Api.Client {
         }
 
         public async Task<ProfileResponse> GetProfileAsync(string profileId) {
+            this.ValidateRequiredUrlParameter(nameof(profileId), profileId);
             return await this.GetAsync<ProfileResponse>($"profiles/{profileId}").ConfigureAwait(false);
         }
 
@@ -28,42 +29,56 @@ namespace Mollie.Api.Client {
         }
 
         public async Task<ProfileResponse> UpdateProfileAsync(string profileId, ProfileRequest request) {
+            this.ValidateRequiredUrlParameter(nameof(profileId), profileId);
             return await this.PostAsync<ProfileResponse>($"profiles/{profileId}", request).ConfigureAwait(false);
         }
 
         public async Task<PaymentMethodResponse> EnablePaymentMethodAsync(string profileId, string paymentMethod) {
+            this.ValidateRequiredUrlParameter(nameof(profileId), profileId);
+            this.ValidateRequiredUrlParameter(nameof(paymentMethod), paymentMethod);
             return await this.PostAsync<PaymentMethodResponse>($"profiles/{profileId}/methods/{paymentMethod}", null).ConfigureAwait(false);
         }
 
         public async Task<PaymentMethodResponse> EnablePaymentMethodAsync(string paymentMethod) {
+            this.ValidateRequiredUrlParameter(nameof(paymentMethod), paymentMethod);
             return await this.PostAsync<PaymentMethodResponse>($"profiles/me/methods/{paymentMethod}", null).ConfigureAwait(false);
         }
 
         public async Task DisablePaymentMethodAsync(string profileId, string paymentMethod) {
+            this.ValidateRequiredUrlParameter(nameof(profileId), profileId);
+            this.ValidateRequiredUrlParameter(nameof(paymentMethod), paymentMethod);
             await this.DeleteAsync($"profiles/{profileId}/methods/{paymentMethod}").ConfigureAwait(false);
         }
 
         public async Task DisablePaymentMethodAsync(string paymentMethod) {
+            this.ValidateRequiredUrlParameter(nameof(paymentMethod), paymentMethod);
             await this.DeleteAsync($"profiles/me/methods/{paymentMethod}").ConfigureAwait(false);
         }
 
         public async Task DeleteProfileAsync(string profileId) {
+            this.ValidateRequiredUrlParameter(nameof(profileId), profileId);
             await this.DeleteAsync($"profiles/{profileId}").ConfigureAwait(false);
         }
 
         public async Task<EnableGiftCardIssuerResponse> EnableGiftCardIssuerAsync(string profileId, string issuer) {
+            this.ValidateRequiredUrlParameter(nameof(profileId), profileId);
+            this.ValidateRequiredUrlParameter(nameof(issuer), issuer);
             return await this.PostAsync<EnableGiftCardIssuerResponse>($"profiles/{profileId}/methods/giftcard/issuers/{issuer}", null).ConfigureAwait(false);
         }
 
         public async Task<EnableGiftCardIssuerResponse> EnableGiftCardIssuerAsync(string issuer) {
+            this.ValidateRequiredUrlParameter(nameof(issuer), issuer);
             return await this.PostAsync<EnableGiftCardIssuerResponse>($"profiles/me/methods/giftcard/issuers/{issuer}", null).ConfigureAwait(false);
         }
 
         public async Task DisableGiftCardIssuerAsync(string profileId, string issuer) {
+            this.ValidateRequiredUrlParameter(nameof(profileId), profileId);
+            this.ValidateRequiredUrlParameter(nameof(issuer), issuer);
             await this.DeleteAsync($"profiles/{profileId}/methods/giftcard/issuers/{issuer}", null).ConfigureAwait(false);
         }
 
         public async Task DisableGiftCardIssuerAsync(string issuer) {
+            this.ValidateRequiredUrlParameter(nameof(issuer), issuer);
             await this.DeleteAsync($"profiles/me/methods/giftcard/issuers/{issuer}", null).ConfigureAwait(false);
         }
     }
