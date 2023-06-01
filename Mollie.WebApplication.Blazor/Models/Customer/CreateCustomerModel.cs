3e4a758efeb307eff03c1f509a8723f4f5f0acb0
Vincent <vincentkok@live.nl>
2023-06-01 22:05:43 +0200

    Implement create customer page

---
+++ Mollie.WebApplication.Blazor/Models/Customer/CreateCustomerModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mollie.WebApplication.Blazor.Models.Customer;
+
+public class CreateCustomerModel {
+    [Required]
+    public string Name { get; set; }
+
+    [EmailAddress]
+    public string Email { get; set; }
+}
