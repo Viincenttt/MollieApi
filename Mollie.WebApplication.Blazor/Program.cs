e1a00e742cd975d948fa1a8e82731671474a0f05
Vincent <vincentkok@live.nl>
2023-06-01 19:28:01 +0200

    Remove template code

--- Mollie.WebApplication.Blazor/Program.cs
+++ Mollie.WebApplication.Blazor/Program.cs
@@ -1,13 +1,8 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-using Mollie.WebApplication.Blazor.Data;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
 
