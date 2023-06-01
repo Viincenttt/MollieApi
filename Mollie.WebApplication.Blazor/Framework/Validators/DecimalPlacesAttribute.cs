d2936ab67c7e3f5fb40fad58a32350a6b8146ea4
Vincent <vincentkok@live.nl>
2023-06-01 21:50:50 +0200

    Implement create payment page

---
+++ Mollie.WebApplication.Blazor/Framework/Validators/DecimalPlacesAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace Mollie.WebApplication.Blazor.Framework.Validators; 
+
+public class DecimalPlacesAttribute : ValidationAttribute {
+    public int DecimalPlaces { get; }
+
+    public DecimalPlacesAttribute(int decimalPlaces) {
+        DecimalPlaces = decimalPlaces;
+    }
+
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext) {
+        decimal amount = (decimal)value;
+        string text = amount.ToString(CultureInfo.InvariantCulture);
+        int dotIndex = text.IndexOf('.');
+        var decimals = text.Length - dotIndex - 1;
+        var places = DecimalPlaces switch
+        {
+            0 => "without decimal places",
+            1 => "with one decimal place",
+            _ => $"with {DecimalPlaces} decimal places"
+        };
+        return dotIndex < 0 || dotIndex != text.LastIndexOf('.') || decimals != DecimalPlaces
+            ? new ValidationResult(ErrorMessage ?? $"Please enter an amount {places}")
+            : ValidationResult.Success;
+    }
+}
