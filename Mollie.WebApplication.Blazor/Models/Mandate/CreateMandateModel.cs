b7e76ba36cb21c95a16181b8e0e9631a2e9950dd
Vincent <vincentkok@live.nl>
2023-06-01 22:23:40 +0200

    Implement create mandate form

---
+++ Mollie.WebApplication.Blazor/Models/Mandate/CreateMandateModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mollie.WebApplication.Blazor.Models.Mandate; 
+
+public class CreateMandateModel {
+    [Required]
+    public string ConsumerName { get; set; }
+    
+    [Required]
+    public string ConsumerAccount { get; set; }
+}
