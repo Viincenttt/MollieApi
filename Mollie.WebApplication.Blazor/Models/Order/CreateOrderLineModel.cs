c430595df3c8094cd00e402db37923c4074e81cb
Vincent <vincentkok@live.nl>
2023-06-02 08:41:57 +0200

    Add order line editor

--- Mollie.WebApplication.Blazor/Models/Order/CreateOrderLineModel.cs
+++ Mollie.WebApplication.Blazor/Models/Order/CreateOrderLineModel.cs
@@ -21,8 +21,9 @@ public class CreateOrderLineModel {
     [DecimalPlaces(2)]
     public decimal TotalAmount { get; set; }
     
-    [Required]
-    public string VatRate { get; set; }
+    [Range(0.01, 100, ErrorMessage = "Please enter a vat rate between 0.01 and 100")]
+    [DecimalPlaces(2)]
+    public decimal VatRate { get; set; }
     
     [Required]
     [Range(0.01, 10000, ErrorMessage = "Please enter a vat amount between 0.01 and 10000")]
