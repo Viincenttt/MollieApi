7d86df92eebbb678d6a5ae141f7a08c6153b927a
Vincent <vincentkok@live.nl>
2023-06-02 08:18:28 +0200

    Fix build warnings

--- Mollie.WebApplication.Blazor/Pages/Error.cshtml.cs
+++ Mollie.WebApplication.Blazor/Pages/Error.cshtml.cs
@@ -7,7 +7,7 @@ namespace Mollie.WebApplication.Blazor.Pages;
 [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 [IgnoreAntiforgeryToken]
 public class ErrorModel : PageModel {
-    public string? RequestId { get; set; }
+    public string RequestId { get; set; }
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
