e7dec515730c150c8f0c76ac5a8aa5cb6363b145
Vincent <vincentkok@live.nl>
2023-06-02 09:26:08 +0200

    Auto calculate some order properties to make editing less tedious

--- Mollie.WebApplication.Blazor/Models/Order/CreateOrderLineModel.cs
+++ Mollie.WebApplication.Blazor/Models/Order/CreateOrderLineModel.cs
@@ -16,17 +16,10 @@ public class CreateOrderLineModel {
     [DecimalPlaces(2)]
     public decimal UnitPrice { get; set; }
     
-    [Required]
-    [Range(0.01, 10000, ErrorMessage = "Please enter a total amount between 0.01 and 10000")]
-    [DecimalPlaces(2)]
     public decimal TotalAmount { get; set; }
     
     [Range(0.01, 100, ErrorMessage = "Please enter a vat rate between 0.01 and 100")]
     [DecimalPlaces(2)]
     public decimal VatRate { get; set; }
-    
-    [Required]
-    [Range(0.01, 10000, ErrorMessage = "Please enter a vat amount between 0.01 and 10000")]
-    [DecimalPlaces(2)]
     public decimal VatAmount { get; set; }
 }
--- Mollie.WebApplication.Blazor/Models/Order/CreateOrderModel.cs
+++ Mollie.WebApplication.Blazor/Models/Order/CreateOrderModel.cs
@@ -10,10 +10,6 @@ public class CreateOrderModel {
     
     [Required]
     public string Locale { get; set; }
-    
-    [Required]
-    [Range(0.01, 1000, ErrorMessage = "Please enter an amount between 0.01 and 1000")]
-    [DecimalPlaces(2)]
     public decimal Amount { get; set; }
 
     [Required]
