bd1040c899339f80f7a697d4293385cbf1ef6222
Vincent <vincentkok@live.nl>
2023-08-26 12:03:23 +0200

    #316 - Add documentation on new dependency injection mechanism

--- src/Mollie.Api/Framework/MollieHttpRetryPolicies.cs
+++ src/Mollie.Api/Framework/MollieHttpRetryPolicies.cs
@@ -5,6 +5,10 @@ using Polly.Extensions.Http;
 
 namespace Mollie.Api.Framework {
     public static class MollieHttpRetryPolicies {
+        /// <summary>
+        /// Retry policy to automatically retry transient errors.
+        /// Transient errors are network failures, http 5xx and http 408 errors.
+        /// </summary>
         public static IAsyncPolicy<HttpResponseMessage> TransientHttpErrorRetryPolicy(int numberOfRetries = 3) {
             return HttpPolicyExtensions
                 .HandleTransientHttpError()
