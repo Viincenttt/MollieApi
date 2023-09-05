1fe38795ac8d2d8cd7d9d38f742d4d6d0c6dc3c7
Vincent <vincentkok@live.nl>
2023-09-05 07:19:05 +0200

    #322 Add pagination to ChargebackClient list method

--- src/Mollie.Api/Client/Abstract/IChargebacksClient.cs
+++ src/Mollie.Api/Client/Abstract/IChargebacksClient.cs
@@ -2,11 +2,13 @@
 using System.Threading.Tasks;
 using Mollie.Api.Models.Chargeback;
 using Mollie.Api.Models.List;
+using Mollie.Api.Models.Url;
 
 namespace Mollie.Api.Client.Abstract {
     public interface IChargebacksClient : IDisposable {
         Task<ChargebackResponse> GetChargebackAsync(string paymentId, string chargebackId, bool testmode = false);
         Task<ListResponse<ChargebackResponse>> GetChargebacksListAsync(string paymentId, string from = null, int? limit = null, bool testmode = false);
         Task<ListResponse<ChargebackResponse>> GetChargebacksListAsync(string profileId = null, bool testmode = false);
+        Task<ListResponse<ChargebackResponse>> GetChargebacksListAsync(UrlObjectLink<ListResponse<ChargebackResponse>> url);
     }
 }
