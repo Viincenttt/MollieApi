069b4ce2d4cc2ca75a47214ba39c88dc500417cd
Vincent <vincentkok@live.nl>
2023-12-22 14:34:26 +0100

    Add missing Dashboard link in ProfileResponse

--- src/Mollie.Api/Models/Profile/Response/ProfileResponseLinks.cs
+++ src/Mollie.Api/Models/Profile/Response/ProfileResponseLinks.cs
@@ -8,6 +8,7 @@ using Mollie.Api.Models.Url;
 namespace Mollie.Api.Models.Profile.Response {
     public class ProfileResponseLinks {
         public UrlObjectLink<ProfileResponse> Self { get; set; }
+        public UrlLink Dashboard { get; set; }
         public UrlObjectLink<ListResponse<ChargebackResponse>> Chargebacks { get; set; }
         public UrlObjectLink<ListResponse<PaymentResponse>> Methods { get; set; }
         public UrlObjectLink<ListResponse<PaymentMethodResponse>> Payments { get; set; }
