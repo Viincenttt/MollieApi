d20e22354c5e00c024a7889cd04c141b0d8025b0
Vincent Kok <vincentkok@live.nl>
2024-03-23 19:59:52 +0100

    Add PolySharp so we can use required and init keyword

--- src/Mollie.Api/Models/Customer/CustomerRequest.cs
+++ src/Mollie.Api/Models/Customer/CustomerRequest.cs
@@ -17,21 +17,21 @@ namespace Mollie.Api.Models.Customer {
         /// Allows you to preset the language to be used in the payment screens shown to the consumer. When this parameter is not 
         /// provided, the browser language will be used instead in the payment flow (which is usually more accurate).
         /// </summary>
-        public string Locale { get; set; }
+        public string? Locale { get; set; }
 
         /// <summary>
         /// Optional - Provide any data you like in JSON notation, and we will save the data alongside the customer. Whenever
         /// you fetch the customer with our API, we'll also include the metadata. You can use up to 1kB of JSON.
         /// </summary>
         [JsonConverter(typeof(RawJsonConverter))]
-        public string Metadata { get; set; }
+        public string? Metadata { get; set; }
         
         /// <summary>
         ///	Oauth only - Optional – Set this to true to make this customer a test customer.
         /// </summary>
         public bool? Testmode { get; set; }
 
-        public void SetMetadata(object metadataObj, JsonSerializerSettings jsonSerializerSettings = null) {
+        public void SetMetadata(object metadataObj, JsonSerializerSettings? jsonSerializerSettings = null) {
             this.Metadata = JsonConvert.SerializeObject(metadataObj, jsonSerializerSettings);
         }
     }
