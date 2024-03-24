ffe205bc6c9580aa6aafa3f3eef8a8a5071e8fa5
Vincent Kok <vincentkok@live.nl>
2024-03-24 20:53:21 +0100

    259 nullability warnings to go

--- src/Mollie.Api/Models/Connect/TokenResponse.cs
+++ src/Mollie.Api/Models/Connect/TokenResponse.cs
@@ -6,32 +6,32 @@ namespace Mollie.Api.Models.Connect {
         ///     The access token, with which you will be able to access the Mollie API on the merchant's behalf.
         /// </summary>
         [JsonProperty("access_token")]
-        public string AccessToken { get; set; }
+        public required string AccessToken { get; init; }
 
         /// <summary>
         ///     The refresh token, with which you will be able to retrieve new access tokens on this endpoint. Please note that the
         ///     refresh token does not expire.
         /// </summary>
         [JsonProperty("refresh_token")]
-        public string RefreshToken { get; set; }
+        public required string RefreshToken { get; init; }
 
         /// <summary>
         ///     The number of seconds left before the access token expires. Be sure to renew your access token before this reaches
         ///     zero.
         /// </summary>
         [JsonProperty("expires_in")]
-        public int ExpiresIn { get; set; }
+        public required int ExpiresIn { get; init; }
 
         /// <summary>
         ///     As per OAuth standards, the provided access token can only be used with bearer authentication.
         ///     Possible values: bearer
         /// </summary>
         [JsonProperty("token_type")]
-        public string TokenType { get; set; }
+        public required string TokenType { get; init; }
 
         /// <summary>
         ///     A space separated list of permissions. Please refer to OAuth: Permissions for the full permission list.
         /// </summary>
-        public string Scope { get; set; }
+        public required string Scope { get; init; }
     }
 }
