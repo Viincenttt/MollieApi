cece38c9d9ca6978baa8d708b4ab55e03afa8b0d
Vincent Kok <vincentkok@live.nl>
2024-03-25 19:55:24 +0100

    181 nullability warnings to go

--- src/Mollie.Api/Models/Mandate/MandateResponse.cs
+++ src/Mollie.Api/Models/Mandate/MandateResponse.cs
@@ -56,41 +56,41 @@ namespace Mollie.Api.Models.Mandate {
         /// <summary>
         /// The direct debit account holder's name.
         /// </summary>
-        public string ConsumerName { get; set; }
+        public string? ConsumerName { get; set; }
 
         /// <summary>
         /// The direct debit account IBAN.
         /// </summary>
-        public string ConsumerAccount { get; set; }
+        public string? ConsumerAccount { get; set; }
 
         /// <summary>
         ///  The direct debit account BIC.
         /// </summary>
-        public string ConsumerBic { get; set; }
+        public string? ConsumerBic { get; set; }
 
         /// <summary>
         /// The credit card holder's name.
         /// </summary>
-        public string CardHolder { get; set; }
+        public string? CardHolder { get; set; }
 
         /// <summary>
         /// The last four digits of the credit card number.
         /// </summary>
-        public string CardNumber { get; set; }
+        public string? CardNumber { get; set; }
 
         /// <summary>
         /// The credit card's label. Note that not all labels can be acquired through Mollie.
         /// </summary>
-        public string CardLabel { get; set; }
+        public string? CardLabel { get; set; }
 
         /// <summary>
         /// Unique alphanumeric representation of credit card, usable for identifying returning customers.
         /// </summary>
-        public string CardFingerprint { get; set; }
+        public string? CardFingerprint { get; set; }
 
         /// <summary>
         /// Expiry date of the credit card card in YYYY-MM-DD format.
         /// </summary>
-        public string CardExpiryDate { get; set; }
+        public string? CardExpiryDate { get; set; }
     }
 }
