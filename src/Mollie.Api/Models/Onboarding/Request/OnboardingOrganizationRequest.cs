465cab51df1ca2c0dbf11608097e2bca77e06fa2
Vincent Kok <vincentkok@live.nl>
2024-03-29 10:36:36 +0100

    Add missing properties in OnboardingOrganizationRequest (#350)

--- src/Mollie.Api/Models/Onboarding/Request/OnboardingOrganizationRequest.cs
+++ src/Mollie.Api/Models/Onboarding/Request/OnboardingOrganizationRequest.cs
@@ -12,5 +12,21 @@
         /// Address of the organization.
         /// </summary>
         public AddressObject Address { get; set; }
+        
+        /// <summary>
+        /// The Chamber of Commerce (or local equivalent) registration number of the organization.
+        /// </summary>
+        public string RegistrationNumber { get; set; }
+        
+        /// <summary>
+        /// The VAT number of the organization, if based in the European Union or the United Kingdom.
+        /// </summary>
+        public string VatNumber { get; set; }
+        
+        /// <summary>
+        /// The organization’s VAT regulation, if based in the European Union. Either shifted (VAT is shifted)
+        /// or dutch (Dutch VAT rate) is accepted.
+        /// </summary>
+        public string VatRegulation { get; set; }
     }
 }
