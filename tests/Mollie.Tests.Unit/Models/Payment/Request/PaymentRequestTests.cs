e4996ac3870e1d935c324f00b5fc026fb998fae7
Vincent <vincentkok@live.nl>
2024-07-09 19:35:25 +0200

    Add unit test to create a payment method specific type

---
+++ tests/Mollie.Tests.Unit/Models/Payment/Request/PaymentRequestTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using FluentAssertions;
+using Mollie.Api.Models;
+using Mollie.Api.Models.Payment;
+using Mollie.Api.Models.Payment.Request;
+using Mollie.Api.Models.Payment.Request.PaymentSpecificParameters;
+using Xunit;
+
+namespace Mollie.Tests.Unit.Models.Payment.Request;
+
+public class PaymentRequestTests {
+    [Theory]
+    [InlineData(PaymentMethod.CreditCard, typeof(CreditCardPaymentRequest))]
+    [InlineData(PaymentMethod.Ideal, typeof(IdealPaymentRequest))]
+    public void CreatePaymentRequest(string paymentMethod, Type expectedType) {
+        var amount = new Amount(Currency.EUR, 50m);
+        var description = "my-description";
+        var paymentRequest = new PaymentRequest() {
+            Amount = amount,
+            Description = description
+        };
+        switch (paymentMethod) {
+            case PaymentMethod.CreditCard:
+                paymentRequest = new CreditCardPaymentRequest(paymentRequest) {
+                    CardToken = "card-token"
+                };
+                break;
+            case PaymentMethod.Ideal:
+                paymentRequest = new IdealPaymentRequest(paymentRequest) {
+                    Issuer = "ideal_issuer"
+                };
+                break;
+        }
+
+        paymentRequest.Should().BeOfType(expectedType);
+        paymentRequest.Amount.Should().Be(amount);
+        paymentRequest.Description.Should().Be(description);
+    }
+}
