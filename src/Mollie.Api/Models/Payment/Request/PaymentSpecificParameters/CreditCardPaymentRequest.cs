5eccbb18ba29f5063471f6d766e307377241b63a
Vincent <vincentkok@live.nl>
2024-07-09 19:11:11 +0200

    Remove billing and shipping address from creditcard payment request

--- src/Mollie.Api/Models/Payment/Request/PaymentSpecificParameters/CreditCardPaymentRequest.cs
+++ src/Mollie.Api/Models/Payment/Request/PaymentSpecificParameters/CreditCardPaymentRequest.cs
@@ -5,18 +5,6 @@ namespace Mollie.Api.Models.Payment.Request.PaymentSpecificParameters {
         }
 
         /// <summary>
-        /// The card holder�s address details. We advise to provide these details to improve the credit card
-        /// fraud protection, and thus improve conversion.
-        /// </summary>
-        public AddressObject? BillingAddress { get; set; }
-
-        /// <summary>
-        /// The shipping address details. We advise to provide these details to improve the credit card fraud
-        /// protection, and thus improve conversion.
-        /// </summary>
-        public AddressObject? ShippingAddress { get; set; }
-
-        /// <summary>
         /// The card token you get from Mollie Components. The token contains the card information
         /// (such as card holder, card number and expiry date) needed to complete the payment.
         /// </summary>
