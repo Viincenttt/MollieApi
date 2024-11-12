7de4b20c028030fe1d71f0bd8ec7a4455bf89518
Vincent Kok <vincentkok@live.nl>
2024-11-12 08:41:19 +0100

    #403 Make currency code constant values instead of readonly (#407)

--- src/Mollie.Api/Models/Currency.cs
+++ src/Mollie.Api/Models/Currency.cs
@@ -1,23 +1,23 @@
 ﻿namespace Mollie.Api.Models {
     public static class Currency {
-        public static string AUD = nameof(AUD);
-        public static string BGN = nameof(BGN);
-        public static string CAD = nameof(CAD);
-        public static string HRK = nameof(HRK);
-        public static string CZK = nameof(CZK);
-        public static string DKK = nameof(DKK);
-        public static string HKD = nameof(HKD);
-        public static string HUF = nameof(HUF);
-        public static string ISK = nameof(ISK);
-        public static string ILS = nameof(ILS);
-        public static string JPY = nameof(JPY);
-        public static string NOK = nameof(NOK);
-        public static string PLN = nameof(PLN);
-        public static string GBP = nameof(GBP);
-        public static string RON = nameof(RON);
-        public static string SEK = nameof(SEK);
-        public static string CHF = nameof(CHF);
-        public static string USD = nameof(USD);
-        public static string EUR = nameof(EUR);
+        public const string AUD = nameof(AUD);
+        public const string BGN = nameof(BGN);
+        public const string CAD = nameof(CAD);
+        public const string HRK = nameof(HRK);
+        public const string CZK = nameof(CZK);
+        public const string DKK = nameof(DKK);
+        public const string HKD = nameof(HKD);
+        public const string HUF = nameof(HUF);
+        public const string ISK = nameof(ISK);
+        public const string ILS = nameof(ILS);
+        public const string JPY = nameof(JPY);
+        public const string NOK = nameof(NOK);
+        public const string PLN = nameof(PLN);
+        public const string GBP = nameof(GBP);
+        public const string RON = nameof(RON);
+        public const string SEK = nameof(SEK);
+        public const string CHF = nameof(CHF);
+        public const string USD = nameof(USD);
+        public const string EUR = nameof(EUR);
     }
 }
