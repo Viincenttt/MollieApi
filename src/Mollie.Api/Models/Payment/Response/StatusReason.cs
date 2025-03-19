1bf812be68df82d81e82d5321284e8129c1b4886
Vincent Kok <vincentkok@live.nl>
2025-03-19 11:31:15 +0100

    Add support for the StatusReason field

---
+++ src/Mollie.Api/Models/Payment/Response/StatusReason.cs
@@ -0,0 +1,13 @@
+﻿namespace Mollie.Api.Models.Payment.Response;
+
+public record StatusReason {
+    /// <summary>
+    /// A machine-readable code that indicates the reason for the payment's status.
+    /// </summary>
+    public required string Code { get; init; }
+
+    /// <summary>
+    /// A machine-readable code that indicates the reason for the payment's status.
+    /// </summary>
+    public required string Message { get; init; }
+}
