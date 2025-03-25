e8d033fe22a281b3720074c379b05dd7433380bf
Vincent Kok <vincentkok@live.nl>
2025-03-25 19:21:18 +0100

    Create invoice models and client

---
+++ src/Mollie.Api/Client/Abstract/ISalesInvoiceClient.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+using Mollie.Api.Models.SalesInvoice.Request;
+using Mollie.Api.Models.SalesInvoice.Response;
+
+namespace Mollie.Api.Client.Abstract;
+
+public interface ISalesInvoiceClient : IDisposable {
+    Task<SalesInvoiceResponse> CreateSalesInvoiceAsync(SalesInvoiceRequest salesInvoiceRequest);
+}
