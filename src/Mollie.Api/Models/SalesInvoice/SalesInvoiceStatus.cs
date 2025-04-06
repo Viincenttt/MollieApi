132f17ec30bebb0d9d525baa4dc6abf10ed8a168
Vincent Kok <vincentkok@live.nl>
2025-04-06 11:40:53 +0200

    Add a list of constant values for SalesInvoiceStatus

---
+++ src/Mollie.Api/Models/SalesInvoice/SalesInvoiceStatus.cs
@@ -0,0 +1,7 @@
+﻿namespace Mollie.Api.Models.SalesInvoice;
+
+public static class SalesInvoiceStatus {
+    public const string Draft = "draft";
+    public const string Issued = "issued";
+    public const string Paid = "paid";
+}
