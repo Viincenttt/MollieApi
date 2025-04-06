c2d18145719cc83e3c6e89f1ff34d14f06697793
Vincent Kok <vincentkok@live.nl>
2025-04-06 13:41:54 +0200

    Add support for UpdateSalesInvoiceAsync method

--- src/Mollie.Api/Client/Abstract/ISalesInvoiceClient.cs
+++ src/Mollie.Api/Client/Abstract/ISalesInvoiceClient.cs
@@ -10,5 +10,6 @@ public interface ISalesInvoiceClient : IDisposable {
     Task<SalesInvoiceResponse> CreateSalesInvoiceAsync(SalesInvoiceRequest salesInvoiceRequest);
     Task<ListResponse<SalesInvoiceResponse>> GetSalesInvoiceListAsync(string? from = null, int? limit = null);
     Task<SalesInvoiceResponse> GetSalesInvoiceAsync(string salesInvoiceId);
+    Task<SalesInvoiceResponse> UpdateSalesInvoiceAsync(string salesInvoiceId, SalesInvoiceUpdateRequest salesInvoiceRequest);
     Task DeleteSalesInvoiceAsync(string salesInvoiceId);
 }
