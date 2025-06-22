8b1cf7f8ed09661defd8d59b8c5f2a08fd57a8c0
Vincent Kok <vincentkok@live.nl>
2025-06-22 09:55:24 +0200

    Add payment link demo to example project (#435)

---
+++ samples/Mollie.WebApplication.Blazor/Models/PaymentLink/CreatePaymentModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Mollie.Api.Models;
+using Mollie.WebApplication.Blazor.Framework.Validators;
+
+namespace Mollie.WebApplication.Blazor.Models.PaymentLink;
+
+public class CreatePaymentLinkModel {
+    [Required]
+    [Range(0.01, 1000, ErrorMessage = "Please enter an amount between 0.01 and 1000")]
+    [DecimalPlaces(2)]
+    public required decimal Amount { get; set; }
+
+    [Required]
+    [StaticStringList(typeof(Currency))]
+    public required string Currency { get; set; }
+
+    [Required]
+    [Url]
+    public required string RedirectUrl { get; set; }
+
+    [Url]
+    public string? WebhookUrl { get; set; }
+
+    [Required]
+    public required string Description { get; set; }
+}
