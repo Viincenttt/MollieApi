72959f38a67f2cd282e20b7a240873d0e68b2caf
Vincent Kok <vincentkok@live.nl>
2025-08-04 19:22:51 +0200

    Add webhook create page

---
+++ samples/Mollie.WebApplication.Blazor/Models/Webhook/CreateWebhookModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mollie.WebApplication.Blazor.Models.Webhook;
+
+public class CreateWebhookModel {
+    [Required]
+    public required string Name { get; set; }
+
+    [Required]
+    [Url]
+    public required string Url { get; set; }
+
+    [Required]
+    public required string EventTypes { get; set; } // TODO: Add a list of valid event types
+
+    public bool Testmode { get; set; } = true;
+}
