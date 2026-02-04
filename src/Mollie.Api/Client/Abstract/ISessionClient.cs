d84f30b1f7f3b4acf61b7a2a67745f7826ddd60a
Francesco Bognini <info@francescobognini.com>
2026-02-04 06:22:23 +0100

    Add SessionClient

---
+++ src/Mollie.Api/Client/Abstract/ISessionClient.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Mollie.Api.Models.List.Response;
+using Mollie.Api.Models.Payment.Response;
+using Mollie.Api.Models.Session.Request;
+using Mollie.Api.Models.Session.Response;
+using Mollie.Api.Models.Url;
+
+namespace Mollie.Api.Client.Abstract {
+    public interface ISessionClient : IBaseMollieClient {
+
+        /// <summary>
+        /// Create a new Session.
+        /// </summary>
+        /// <param name="request">The Session request object containing the Session details</param>
+        /// <param name="cancellationToken">Token to cancel the request</param>
+        /// <returns>The Session object created by Mollie</returns>
+        Task<SessionResponse> CreateSessionAsync(SessionRequest request, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Retrieve a single Session by its ID.
+        /// </summary>
+        /// <param name="sessionId">The Session ID of the Session to retrieve</param>
+        /// <param name="testmode">Indicates whether the Session is in test mode or not</param>
+        /// <param name="cancellationToken">Token to cancel the request</param>
+        /// <returns>The Session object retrieved by Mollie</returns>
+        Task<SessionResponse> GetSessionAsync(string sessionId, bool testmode = false, CancellationToken cancellationToken = default);
+
+    }
+}
---
+++ src/Mollie.Api/Models/Session/Response/SessionPaymentResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text.Json.Serialization;
+using Mollie.Api.JsonConverters;
+
+namespace Mollie.Api.Models.Session.Response;
+
+public record SessionPaymentResponse {
+
+    /// <summary>
+    /// The webhook URL where we will send payment status updates to.
+    /// This URL will be automatically set as the webhook URL for all payments created for this session.
+    /// </summary>
+    public string? WebhookUrl { get; set; }
+}
