22c76448b802966f3a1d575523d163de2b6106dc
Francesco Bognini <info@francescobognini.com>
2026-02-09 12:55:24 +0100

    Remove all links but self from SessionResponseLinks

--- src/Mollie.Api/Models/Session/Response/SessionResponseLinks.cs
+++ src/Mollie.Api/Models/Session/Response/SessionResponseLinks.cs
@@ -10,26 +10,5 @@ namespace Mollie.Api.Models.Session.Response {
         ///     The API resource URL of the session itself.
         /// </summary>
         public required UrlObjectLink<SessionResponse> Self { get; set; }
-
-        /// <summary>
-        /// The API resource URL of the customer the session is for.
-        /// </summary>
-        public required UrlObjectLink<CustomerResponse> Customer { get; set; }
-
-        /// <summary>
-        /// The API resource URL of the payments that are created by this session. Not present
-        /// if no payments yet created.
-        /// </summary>
-        public UrlObjectLink<ListResponse<PaymentResponse>>? Payments { get; set; }
-
-        /// <summary>
-        /// The API resource URL of the website profile on which this session was created.
-        /// </summary>
-        public required UrlObjectLink<ProfileResponse> Profile { get; set; }
-
-        /// <summary>
-        /// The URL to the session retrieval endpoint documentation.
-        /// </summary>
-        public required UrlLink Documentation { get; set; }
     }
 }
